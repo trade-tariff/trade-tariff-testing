71127994da862e31f0355a7d2654a9bed23e092a
Madhu Dola <madviv@hotmail.com>
2020-12-14 17:32:12 +0000

    changed baseUrl

--- cypress/integration/LegalBaseColumn-XI.spec.js
+++ cypress/integration/LegalBaseColumn-XI.spec.js
@@ -1,13 +1,18 @@
 describe('Hide Legal base Column - (XI version) *** Change URLs *** ',function() {
 //HOT-58 Suppressing Legal Base Column for XI
-    Cypress.config('baseUrl','http://localhost:3001')
+  //  Cypress.config('baseUrl','http://localhost:3001')
+    Cypress.config('baseUrl','https://www.trade-tariff.service.gov.uk')
+
     it('1.Imports measures tab on XI - commodity 0101210000', function () {
         cy.visit('/commodities/0101210000#import')
         cy.get('.govuk-tabs__panel').should('not.have.value', 'Legal base')
     })
+
     it('2.Exports measures tab on XI - commodity 0101210000', function () {
-        cy.visit('commodities/0101210000#export')
+        cy.visit('/commodities/0101210000#export')
         cy.wait(1000)
         cy.get('.govuk-tabs__panel').should('not.have.value', 'Legal base')
     })
+
+
 })
