95de21cdde20678dbf8e9671c4a42b3d914d4070
Madhu Dola <madviv@hotmail.com>
2020-12-16 17:02:12 +0000

    dataValidation test added

---
+++ cypress/integration/UK/API/V1/legalBaseV1api-UK.spec.js
@@ -0,0 +1,11 @@
+describe('UK version v1 api - legal base should be present in the backend API response  API',function() {
+
+    Cypress.config('baseUrl', 'https://dev.trade-tariff.service.gov.uk/uk')
+
+    it('v2 - 200, legal base is present', () => {
+        cy.request('/api/v1/commodities/0101210000.json').then((response) => {
+            expect(response).to.have.property('status', 200)
+            expect(response.body.included[285]).to.have.property('type', 'legal_act')
+        })
+    })
+})
--- cypress/integration/XI/API/V1/legalBaseV1api-XI.spec.js
+++ cypress/integration/XI/API/V1/legalBaseV1api-XI.spec.js
@@ -1,9 +1,9 @@
-describe('version v2 api - legal base should be present in the API response backend API',function() {
+describe('version v1 api - legal base should be present in the backend API response  API',function() {
 
     Cypress.config('baseUrl', 'https://dev.trade-tariff.service.gov.uk/xi')
 
     it('v2 - 200, legal base is present', () => {
-        cy.request('/api/v2/commodities/0101210000.json').then((response) => {
+        cy.request('/api/v1/commodities/0101210000.json').then((response) => {
             expect(response).to.have.property('status', 200)
             expect(response.body.included[285]).to.have.property('type', 'legal_act')
         })
