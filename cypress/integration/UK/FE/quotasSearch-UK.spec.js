893c953d449668a3c013ef8e59db612618c2eae8
Madhu Dola <madviv@hotmail.com>
2021-01-25 19:14:56 +0000

    quotasearch tests

--- cypress/integration/UK/FE/quotasSearch-UK.spec.js
+++ cypress/integration/UK/FE/quotasSearch-UK.spec.js
@@ -1,7 +1,7 @@
 describe('🇬🇧 💡 QuotasSearch)',function() {
     Cypress.config('baseUrl')
 
-    it('Quotas search - Commodity Code',function(){
+    it('Quotas Search - Commodity Code',function(){
         cy.visit('/quota_search')
         cy.contains('Search the Quotas')
         cy.get('input#goods_nomenclature_item_id')
@@ -26,13 +26,101 @@ describe('🇬🇧 💡 QuotasSearch)',function() {
             }
              else{}
     })
-    it('Quotas Seach - No Input',function(){
+    it('Quotas Search - No Input',function(){
         cy.visit('/quota_search')
         cy.contains('Search the Quotas')
         cy.get('form#new_search > input[name=\'new_search\']').click()
         cy.contains('Sorry, there is a problem with the search query. Please specify one or more search criteria.')
 
     })
+    it('Quotas Search - Country list - 🇲🇦 Results',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        cy.get('.js-quota-country-picker').click()
+        cy.wait(1000)
+        cy.get('input#geographical_area_id').type('Morocco (MA)')
+        cy.wait(1000)
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+        cy.get('.govuk-table__row')
+            .contains(' Morocco (MA)')
 
+    })
+    it('Quotas Search - Country list - 🇧🇫 No results',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        cy.get('.js-quota-country-picker').click()
+        cy.wait(1000)
+        cy.get('input#geographical_area_id').type('Burkina Faso (BF)')
+        cy.wait(1000)
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+         cy.contains('There are no matching results')
+
+    })
+    it('Quotas Search - 🇨🇭 Reset to all countries',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        cy.get('.js-quota-country-picker').click()
+        cy.wait(200)
+        cy.get('input#geographical_area_id').type('Switzerland (CH)')
+        cy.wait(200)
+        //reset to all countries
+        cy.get('.reset-country-picker').click()
+        cy.wait(200)
+       cy.get('form#new_search > input[name=\'new_search\']').click()
+
+        cy.contains('Sorry, there is a problem with the search query. Please specify one or more search criteria.')
+    })
+
+    it('Quotas Search - Order Number',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        cy.get('input#order_number')
+            .click().clear().type('057140')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+        cy.get('.govuk-table__row').contains('057140')
+
+    })
+
+    it.skip('Quotas Search - Critical state',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+
+
+    })
+
+    it('Quotas Search - Status',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        //Blocked
+        cy.get('select#status').select('Blocked')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('There are no matching results')
+        //Exhausted
+        cy.get('select#status').select('Exhausted')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+        //Not blocked
+        cy.get('select#status').select('Not blocked')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+        //Not exhausted
+        cy.get('select#status').select('Not exhausted')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+
+    })
+
+    it('Quotas Search - Years',function(){
+        cy.visit('/quota_search')
+        cy.contains('Search the Quotas')
+        cy.get('select#years_').select('2020')
+        cy.get('select#status').select('Not exhausted')
+        cy.get('form#new_search > input[name=\'new_search\']').click()
+        cy.contains('Quota search results')
+
+
+    })
 
 })
