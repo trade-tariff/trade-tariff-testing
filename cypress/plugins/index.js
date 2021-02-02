7cb0368127894f30668b2a28c268d72934afc71c
Madhu Dola <madviv@hotmail.com>
2021-02-02 13:01:14 +0000

    plugins modified

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,9 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
+
 module.exports = (on, config) => {
+  const cucumber = require('cypress-cucumber-preprocessor').default;
   on('file:preprocessor', cucumber())
-}
-const {JsonSchemaValidation} = require('@jc21/cypress-jsonschema-validation')
-module.exports = (on, config) => {
+  const {JsonSchemaValidation} = require('@jc21/cypress-jsonschema-validation');
   on('task', JsonSchemaValidation(config))
   return config
+
 }
-
-
