338c7958f5178318799411fbc5f24f8e0a0bfa9c
Madhu Dola <madviv@hotmail.com>
2021-03-30 16:59:07 +0100

    cookies tests

--- cypress/integration/HOTT-Shared/cookiesTest.spec.js
+++ cypress/integration/HOTT-Shared/cookiesTest.spec.js
@@ -1,12 +1,96 @@
-describe.skip('🇬🇧 🇪🇺 💡 | CookiesTest |',function() {
+
+describe('🇬🇧 🇪🇺 💡 | 🍪 CookiesTest |',function() {
     //  Cypress.config('baseUrl')
         Cypress.config('baseUrl')
   
-      it('Commodity Search - New codes',function(){
-          cy.visit('/sections')
-         cy.log(cy.getCookie('authenticity_token'))
+      it('Cookies Main Banner ',function(){
+        cy.visit('/cookies')
+        cy.contains('Cookies on HMRC services')
+        cy.contains('We use some essential cookies to make our services work.')
+        cy.contains('We would like to set additional cookies so we can remember your settings, understand how people use our services and make improvements.')
+        cy.contains('Accept additional cookies')
+        cy.contains('Reject additional cookies')
+        cy.contains('View cookies')
+      })
+      it('No cookies selection made',function(){
+        cy.visit('/sections')
+        cy.getCookie('cookies_policy').should('eq', null)
+        cy.getCookie('cookies_preferences_set').should('eq', null)
+      })
+
+      it('Cookies on all pages',function(){
+        cy.clearCookies()
+      let pages = ['sections','a-z-index/a','tools','xi/sections','xi/a-z-index/a','xi/tools']
+        for (let i =0 ; i< pages.length ; i++)
+        {
+        cy.visit(`/${pages[i]}`)
+        cy.contains('Cookies on HMRC services')
+        cy.contains('We use some essential cookies to make our services work.')
+        cy.contains('We would like to set additional cookies so we can remember your settings, understand how people use our services and make improvements.')
+        cy.contains('Accept additional cookies')
+        cy.contains('Reject additional cookies')
+        cy.contains('View cookies')
+        }
+    
+      })
+      
+      it('Accept Cookies ,Hide Banner',function(){
+        cy.visit('/sections')
+        cy.get('.govuk-button.cookie_accept_all').wait(200).click().wait(200)
+        cy.contains('Hide this message').click()
+        cy.getCookie('cookies_policy').should('have.property','value','%7B%22settings%22%3Atrue%2C%22usage%22%3A%22true%22%2C%22remember_settings%22%3A%22true%22%7D');
+        cy.getCookie('cookies_preferences_set').should('have.property','value','true')
+      })
+      
+      
+      it('Reject Cookies ,Hide Banner',function(){
+        cy.clearCookies()
+        cy.visit('/sections')
+        cy.get('.govuk-button.cookie_reject_all').wait(200).click().wait(500)
+        cy.contains('Hide this message').click()
+        cy.getCookie('cookies_policy').should('have.property','value',
+        '%7B%22settings%22%3Atrue%2C%22usage%22%3A%22false%22%2C%22remember_settings%22%3A%22false%22%7D')
+        cy.getCookie('cookies_preferences_set').should('have.property','value','true')
 
       })
+
+      it('View Cookies - Yes / No Selection - combinations ',function(){
+        cy.clearCookies()
+        cy.visit('/sections')
+        // view cookies
+        cy.get('.banner-govuk-link.govuk-link').click()
+        cy.contains('Cookies on the UK Global Online Tariff')
+        // Yes / No selection
+        cy.get('input#cookie_consent_usage_true').click()
+        cy.get("input[name='commit']").click()
+        cy.contains('Hide this message').click()
+        cy.contains('Your cookie settings were saved')
+        cy.contains('You can update these settings at any time.')
+        cy.getCookie('cookies_policy').should('have.property','value',
+        '%7B%22settings%22%3Atrue%2C%22usage%22%3A%22true%22%7D')
+        cy.getCookie('cookies_preferences_set').should('have.property','value','true')
+
+
+      })
+      it('View Cookies - No / Yes - combinations ',function(){
+        cy.clearCookies()
+        cy.visit('/sections')
+        // view cookies
+        cy.get('.banner-govuk-link.govuk-link').click()
+        cy.contains('Cookies on the UK Global Online Tariff')
+        // No /Yes selection
+        cy.get("input#cookie_consent_usage_false").click()
+        cy.get("input#cookie_remember_settings_true").click()
+        cy.get("input[name='commit']").click()
+        cy.contains('Hide this message').click()
+        cy.contains('Your cookie settings were saved')
+        cy.contains('You can update these settings at any time.')
+        cy.getCookie('cookies_policy').should('have.property','value',
+        '%7B%22settings%22%3Atrue%2C%22usage%22%3A%22false%22%2C%22remember_settings%22%3A%22true%22%7D')
+        cy.getCookie('cookies_preferences_set').should('have.property','value','true')
+      
 })
 
 
+
+})
