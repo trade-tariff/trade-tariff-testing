62a523252df4218eadfad22a1b4c7ba8d75e4ff3
Madhu Dola <madviv@hotmail.com>
2021-04-16 10:30:22 +0100

    dc custom commands

---
+++ cypress/integration/DutyCalculator/2.RoW-GB/RoW-GB204-e2e.spec.js
@@ -0,0 +1,19 @@
+
+describe('|RoW-GB204-e2e.spec | 🇸🇬 Singapore to 🇬🇧 GB  | 201-e2e.spec | ',function(){
+    Cypress.config('baseUrl', Cypress.config('services')['dutycal'])
+    
+
+    it(`e2e RoW to GB 🇸🇬 Singapore  to 🇬🇧 GB`,function(){
+        //select future date 
+        cy.visit(`/import-date?referred_service=uk&commodity_code=0702000007`)
+        cy.wait(700)
+        cy.enterDate({day:'01',month:'03',year:'2040'})
+        cy.selectDestinationXI()
+        cy.selectSourceGB()
+        cy.traderSchemeNo()
+        cy.certificateNo()
+        cy.monetaryValue({monetary:'500.00',shipping:'100.00',cost:'250.00'})
+        cy.quantity({deca:'1'})
+    })
+
+ })
