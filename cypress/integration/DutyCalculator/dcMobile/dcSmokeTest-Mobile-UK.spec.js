a3d65b47ed0255e064db26b1edd47c2e787114d0
Madhu Dola <madviv@hotmail.com>
2021-05-19 17:37:17 +0100

    duty calculator tests refactor

--- cypress/integration/DutyCalculator/dcMobile/dcSmokeTest-Mobile-UK.spec.js
+++ cypress/integration/DutyCalculator/dcMobile/dcSmokeTest-Mobile-UK.spec.js
@@ -1,5 +1,5 @@
 
-describe('🧮 📱 UK 🇬🇧 💡 | dcSmokeTest-Mobile-UK |Duty Calculator Smoke |',function() {
+describe('🛃 📱 UK 🇬🇧 💡 | dcSmokeTest-Mobile-UK |Duty Calculator Smoke |',function() {
     Cypress.config('baseUrl', Cypress.config('services')['dutycal'])
  //   let country = ["uk"] 
  //   let pagetitles = ["UK Global Online Tariff","Northern Ireland Online Tariff"]
@@ -8,6 +8,104 @@ describe('🧮 📱 UK 🇬🇧 💡 | dcSmokeTest-Mobile-UK |Duty Calculator Sm
     // iphone 
     it(`📱 iphone - e2e journey`, function () {
         cy.viewport('iphone-6', 'landscape')
+        cy.visit(`uk/0702000007/import-date`)
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+        cy.validDate()
+        //destination
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+        cy.selectDestination('xi')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+    //origin
+        cy.selectOrigin('gb')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+        // ✅ Trader Scheme Registered - Yes 
+        cy.traderScheme('yes')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+        // ✅  Final use in NI - Yes 
+        cy.finalUse('yes')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+
+        // 🚫 Non processing - No - The goods will be processed for commercial purposes other than those listed above
+        cy.get("#wizard-steps-planned-processing-planned-processing-commercial-purposes-field").check()
+        cy.contains('Continue').click()
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+
+        //  🚫 Certified as UK Origin
+        cy.certificate('no')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+
+        // Monetary value page 
+        cy.customsValue({monetary:'5000.50',shipping:'455.7533',cost:'4545.987654'})
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+             
+        // Measure amount page 
+        cy.quantity({dtn:'23.98'})
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+        
+
+        //Check your answers page 
+        cy.contains('Check your answers')
+        
+        cy.get('.govuk-button').click()
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
+
+
+        //Final Page 
+        cy.contains('Option 1: Third-country duty')
+        cy.contains('Option 2: Tariff preference - United Kingdom (excluding Northern Ireland)')
+        cy.contains('Option 3: Claiming a waiver – Exchange rate')
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')     
+        })
+
+//android
+it(`📱 android - e2e journey `, function () {
+        cy.viewport('samsung-note9')
         cy.visit(`uk/0702000007/import-date`)
         cy.get('.govuk-header__menu-button').click()
         cy.contains('A-Z')
@@ -66,28 +164,26 @@ describe('🧮 📱 UK 🇬🇧 💡 | dcSmokeTest-Mobile-UK |Duty Calculator Sm
         cy.contains('Search or browse the Tariff')
 
 
-       // Monetary value page 
-       cy.customsValue({monetary:'5000.50',shipping:'455.7533',cost:'4545.987654'})
-       cy.get('.govuk-header__menu-button').click()
-       cy.contains('A-Z')
-       cy.contains('Tools')
-       cy.contains('Search or browse the Tariff')
-
-             
-       // Measure amount page 
-       cy.quantity({dtn:'23.98'})
-       cy.get('.govuk-header__menu-button').click()
+        // Monetary value page 
+        cy.customsValue({monetary:'5000.50',shipping:'455.7533',cost:'4545.987654'})
+        cy.get('.govuk-header__menu-button').click()
         cy.contains('A-Z')
         cy.contains('Tools')
         cy.contains('Search or browse the Tariff')
 
-     
+                
+        // Measure amount page 
+        cy.quantity({dtn:'23.98'})
+        cy.get('.govuk-header__menu-button').click()
+        cy.contains('A-Z')
+        cy.contains('Tools')
+        cy.contains('Search or browse the Tariff')
 
-       //Check your answers page 
-       cy.contains('Check your answers')
-       cy.get('.govuk-grid-column-three-quarters')
-       cy.get('.govuk-button').click()
-       cy.get('.govuk-header__menu-button').click()
+        //Check your answers page 
+        cy.contains('Check your answers')
+        
+        cy.get('.govuk-button').click()
+        cy.get('.govuk-header__menu-button').click()
         cy.contains('A-Z')
         cy.contains('Tools')
         cy.contains('Search or browse the Tariff')
@@ -101,105 +197,7 @@ describe('🧮 📱 UK 🇬🇧 💡 | dcSmokeTest-Mobile-UK |Duty Calculator Sm
         cy.contains('A-Z')
         cy.contains('Tools')
         cy.contains('Search or browse the Tariff')     
-    })
-
-//android
-it(`📱 android - e2e journey `, function () {
-    cy.viewport('samsung-note9')
-    cy.visit(`uk/0702000007/import-date`)
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-    cy.validDate()
-    //destination
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-    cy.selectDestination('xi')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-    //origin
-    cy.selectOrigin('gb')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-    // ✅ Trader Scheme Registered - Yes 
-    cy.traderScheme('yes')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-    // ✅  Final use in NI - Yes 
-    cy.finalUse('yes')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-
-    // 🚫 Non processing - No - The goods will be processed for commercial purposes other than those listed above
-    cy.get("#wizard-steps-planned-processing-planned-processing-commercial-purposes-field").check()
-    cy.contains('Continue').click()
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-
-    //  🚫 Certified as UK Origin
-    cy.certificate('no')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-
-   // Monetary value page 
-   cy.customsValue({monetary:'5000.50',shipping:'455.7533',cost:'4545.987654'})
-   cy.get('.govuk-header__menu-button').click()
-   cy.contains('A-Z')
-   cy.contains('Tools')
-   cy.contains('Search or browse the Tariff')
-
-         
-   // Measure amount page 
-   cy.quantity({dtn:'23.98'})
-   cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
- 
-
-   //Check your answers page 
-   cy.contains('Check your answers')
-   cy.get('.govuk-grid-column-three-quarters')
-   cy.get('.govuk-button').click()
-   cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')
-
-
-    //Final Page 
-    cy.contains('Option 1: Third-country duty')
-    cy.contains('Option 2: Tariff preference - United Kingdom (excluding Northern Ireland)')
-    cy.contains('Option 3: Claiming a waiver – Exchange rate')
-    cy.get('.govuk-header__menu-button').click()
-    cy.contains('A-Z')
-    cy.contains('Tools')
-    cy.contains('Search or browse the Tariff')     
- 
+    
 })
 
 })
