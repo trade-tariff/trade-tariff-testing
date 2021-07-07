9bd49b9357f0c63c436ef7310dc5f6127c1f6708
Madhu Dola <madviv@hotmail.com>
2021-07-08 00:36:26 +0100

    delta suspensions , quotas

---
+++ cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI304d-delta-e2e.spec.js
@@ -0,0 +1,36 @@
+//More than one Measure amounts
+describe('| Row-NI304d-delta.spec.js | 🔼 Delta Route | more than one measure amounts on UK and XI service | ', function () {
+
+    Cypress.config('baseUrl', Cypress.config('services')['dutycal'])
+    it('RoW 🇦🇪 (United Arab Emirates) - GB', function () {
+        cy.visit('uk/1701141000/import-date')
+        //date
+        cy.validDate()
+        //destination
+        cy.selectDestination('xi')
+        //origin
+        cy.selectOrigin('other')
+        //select country from list 
+        cy.wait(100)
+        cy.otherOriginList({ value: 'United Arab Emirates' })
+        cy.wait(100)
+        //Trader Scheme
+        cy.traderScheme('yes')
+        // ✅  Final use in NI - Yes 
+        cy.finalUseNI('yes')
+        //Planned processing - commercial 
+        cy.plannedXI('commercial')
+        //customs value
+        cy.customsValue({ monetary: '500.00', shipping: '250.00', cost: '250.00' })
+
+        //Import Quantity 
+        cy.quantity({ dtnr: '100',tne:'1', dap:'1' })
+        cy.confirmPage()
+        cy.dutyPage()
+        cy.contains('Option 1: Third-country duty')
+        cy.contains('Third-country duty (EU)')
+        cy.contains('EU import duties apply, as the difference between the UK third country duty and the EU third country duty exceeds 3% of the customs value of your trade.')
+        
+
+    })
+})
---
+++ cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI304e-delta-e2e.spec.js
@@ -0,0 +1,89 @@
+// Preferential Tariff rates 
+//1701141000 - 
+describe('| Row-NI304e-delta.spec.js | 🔼 Delta Route | preferential rates UK and EU  | ', function () {
+
+    Cypress.config('baseUrl', Cypress.config('services')['dutycal'])
+    it('RoW 🇨🇦 (Canada) - GB |UK - yes, EU - no|', function () {
+        cy.visit('xi/0102291010/import-date')
+        //date
+        cy.validDate()
+        //destination
+        cy.selectDestination('xi')
+        //origin
+        cy.selectOrigin('other')
+        //select country from list 
+        cy.wait(100)
+        cy.otherOriginList({ value: 'Canada' })
+        cy.wait(100)
+        //Trader Scheme
+        cy.traderScheme('yes')
+        // ✅  Final use in NI - Yes 
+        cy.finalUseNI('yes')
+        //Planned processing - commercial 
+        cy.plannedXI('commercial')
+        //customs value
+        cy.customsValue({ monetary: '500.00', shipping: '250.00', cost: '250.00' })
+
+        //Import Quantity 
+        cy.quantity({ dtn: '100'})
+        cy.confirmPage()
+        cy.dutyPage()
+        cy.contains('Option 1: Third-country duty')
+        cy.contains('Third-country duty (EU)')
+        cy.contains('EU import duties apply, as the difference between the UK third country duty and the EU third country duty exceeds 3% of the customs value of your trade.')
+        cy.contains('Option 2: Tariff preference - Canada')
+        cy.contains('Tariff preference (UK)')
+        cy.contains("UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.")
+    })
+    //
+    it('RoW 🇹🇷(Turkey) - GB |UK - yes, EU - yes|Qty 1 => UK ,Qty 100 => EU', function () {
+        cy.visit('xi/0102291010/import-date')
+        //date
+        cy.validDate()
+        //destination
+        cy.selectDestination('xi')
+        //origin
+        cy.selectOrigin('other')
+        //select country from list 
+        cy.wait(100)
+        cy.otherOriginList({ value: 'Turkey' })
+        cy.wait(100)
+        //Trader Scheme
+        cy.traderScheme('yes')
+        // ✅  Final use in NI - Yes 
+        cy.finalUseNI('yes')
+        //Planned processing - commercial 
+        cy.plannedXI('commercial')
+        //customs value
+        cy.customsValue({ monetary: '500.00', shipping: '250.00', cost: '250.00' })
+
+        //Import Quantity 
+        cy.quantity({ dtn: '1' })
+        cy.confirmPage()
+        cy.dutyPage()
+        cy.contains('Option 1: Third-country duty')
+        cy.contains('Third-country duty (UK)')
+        cy.contains('UK import duties apply, as the difference between the UK third country duty and the EU third country duty is lower than 3% of the customs value of your trade.')
+        cy.contains('Option 2: Tariff preference - Turkey')
+        cy.contains('Tariff preference (UK)')
+        cy.contains("UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.")
+
+        //Change quantity to 100 for EU tariffs , Delta Preferential > 3% Import Value
+        cy.get('.govuk-back-link').click()
+        cy.get('div:nth-of-type(9) > .govuk-summary-list__actions > .govuk-link').click()
+        //Import Quantity 
+        cy.quantity({ dtn: '100' })
+        cy.confirmPage()
+        cy.dutyPage()
+        cy.contains('Option 1: Third-country duty')
+        cy.contains('Third-country duty (EU)')
+        cy.contains("EU import duties apply, as the difference between the UK third country duty and the EU third country duty exceeds 3% of the customs value of your trade.")
+        cy.contains('Option 2: Tariff preference - Turkey')
+        cy.contains('Tariff preference (EU)')
+        cy.contains("EU preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty exceeds 3% of the customs value of your trade.")
+
+
+
+    })
+
+})
