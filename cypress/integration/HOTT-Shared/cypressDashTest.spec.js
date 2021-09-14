e16ba878d34c28a0f558b540dea23f55a42e3519
Madhu Dola <madviv@hotmail.com>
2021-09-14 17:00:52 +0100

    fix cypressdashboard spec

--- cypress/integration/HOTT-Shared/cypressDashTest.spec.js
+++ cypress/integration/HOTT-Shared/cypressDashTest.spec.js
@@ -4,14 +4,27 @@ describe('Cypress Dashboard',function(){
     it('Cypress Dashboard from GHA',function(){
         cy.wait(100)
         console.log(`${todaysDate}`)
-        cy.visit(`https://trade-tariff.github.io/trade-tariff-testing/${todaysDate}/`, 'failOnStatusCode: false')
-       
-        cy.scrollTo('bottom', { easing: 'linear' })
-        cy.wait(2000)
-        cy.get("li[title='Failed']  .material-icons.quick-summary--circle-icon---1HDS7.quick-summary--icon---TW1oG").click()
-        cy.wait(2000)
-        cy.scrollTo('bottom', { easing: 'linear' })
-        cy.wait(2000)
+        cy.visit({ url: `https://trade-tariff.github.io/trade-tariff-testing/${todaysDate}/`, failOnStatusCode: false })
+
+        let title = cy.title()
+        console.log(title)
+        console.log(cy.title())
+
+        if (title == 'Mochawesome Report'){
+            
+            cy.scrollTo('bottom', { easing: 'linear' })
+            cy.wait(2000)
+            cy.get("li[title='Failed']  .material-icons.quick-summary--circle-icon---1HDS7.quick-summary--icon---TW1oG").click()
+            cy.wait(2000)
+            cy.scrollTo('bottom', { easing: 'linear' })
+            cy.wait(2000)
+        }
+        else {
+            window.close();
+
+        }
+    
+        
     })
     
 })
