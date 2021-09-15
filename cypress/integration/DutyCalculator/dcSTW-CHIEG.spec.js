a195b75dd84b2eedd02da2939747c876defabdf2
M Dola <52670394+madviv@users.noreply.github.com>
2021-09-15 17:39:32 +0100

    Update dcSTW-CHIEG.spec.js
    extra / on cy.visit URL

--- cypress/integration/DutyCalculator/dcSTW-CHIEG.spec.js
+++ cypress/integration/DutyCalculator/dcSTW-CHIEG.spec.js
@@ -1,7 +1,7 @@
 // duty calculator link to STW and CHIEG services
 describe('dcSTW-CHIEG.spec', function() {
   it('1.EU->GB ', function() {
-    cy.visit('//duty-calculator/prefill?commodity_code=0702000007&country_of_origin=FR&import_date=2021-12-21&import_destination=UK');
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=FR&import_date=2021-12-21&import_destination=UK');
     cy.contains('What is the customs value of this import?');
     cy.contains('About this commodity code').click();
     cy.get('.govuk-details__text');
