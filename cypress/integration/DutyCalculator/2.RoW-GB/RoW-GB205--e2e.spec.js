0bcaaf70867405a660f1843642dd468a5ce59a00
Madhu Dola <madviv@hotmail.com>
2021-09-17 11:53:56 +0100

    regression fixes

--- cypress/integration/DutyCalculator/2.RoW-GB/RoW-GB205--e2e.spec.js
+++ cypress/integration/DutyCalculator/2.RoW-GB/RoW-GB205--e2e.spec.js
@@ -1,29 +1,26 @@
 
-describe('|*RoW-GB205--e2e.spec | 🇸🇬 Singapore to 🇬🇧 GB  |',function(){
-   
-    it(`e2e RoW to GB 🇸🇬 Singapore  to 🇬🇧 GB`,function(){
-        cy.visit('/xi/commodities/1701141000')
-        cy.contains('Northern Ireland Online Tariff')
-        cy.get('.govuk-grid-row.import-and-export-boxes .govuk-link').click()
-    
-        cy.wait(700)
-        cy.validDate()
-        cy.selectDestination('gb')
-        cy.originList({value:'Singapore'})
-        cy.customsValue({monetary:'500.00',shipping:'100.00',cost:'250.00'})
-        cy.quantity({tne:'1',dtnr:'1',dap:'1'})
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        cy.confirmPage()
-        cy.dutyPage()
-    
-        cy.contains('Option 1: Third-country duty')
-        cy.contains('Option 2: Tariff preference - Singapore')
-        cy.contains('Option 3: Non-preferential quota 057713')
-        cy.contains('Option 4: Non-preferential tariff quota under end-use 054320')
-    
-    })
+describe('|*RoW-GB205--e2e.spec | 🇸🇬 Singapore to 🇬🇧 GB  |', function() {
+  it(`e2e RoW to GB 🇸🇬 Singapore  to 🇬🇧 GB`, function() {
+    cy.visit('/xi/commodities/1701141000');
+    cy.contains('Northern Ireland Online Tariff');
+    cy.get('.govuk-grid-row.import-and-export-boxes .govuk-link').click();
 
- })
+    cy.wait(700);
+    cy.validDate();
+    cy.selectDestination('gb');
+    cy.originList({value: 'Singapore'});
+    cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
+    cy.quantity({tne: '1', dtnr: '1', dap: '1'});
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Option 2: Tariff preference - Singapore');
+    cy.contains('Option 3: Non-preferential quota 057713');
+    cy.contains('Option 4: Non-preferential tariff quota under end-use 054320');
+  });
+});
--- cypress/integration/DutyCalculator/dcSTW-CHIEG.spec.js
+++ cypress/integration/DutyCalculator/dcSTW-CHIEG.spec.js
@@ -1,6 +1,9 @@
-// duty calculator link to STW and CHIEG services
-describe('dcSTW-CHIEG.spec', function() {
-  it('1.EU->GB ', function() {
+
+// date , commodity ,destination,origin to be persisted on back links
+// back button from date page should take to STW page ( to be confirmed )
+describe('dcSTW-CHIEG.spec |duty calculator link to STW and CHIEG services|', function() {
+  //
+  it('1.EU-GB | Customs value |', function() {
     cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=FR&import_date=2021-12-21&import_destination=UK');
     cy.contains('What is the customs value of this import?');
     cy.contains('About this commodity code').click();
@@ -16,5 +19,22 @@ describe('dcSTW-CHIEG.spec', function() {
     cy.dutyPage();
     cy.contains('Details of your trade');
     cy.contains('21 December 2021');
+  });
+  it.only('2.NI-GB | There is no import duty to pay |', function() {
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=XI&import_date=2021-01-01&import_destination=UK');
+    cy.get('.govuk-back-link').click()
+    
+  });
+  it('3.ROW-GB | Customs value |', function() {
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=NI&import_date=2021-01-01&import_destination=UK');
+  });
+  it('4.EU-XI | There is no import duty to pay |', function() {
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=FI&import_date=2021-01-01&import_destination=XI');
+  });
+  it('5.ROW-XI | Are you authorised under the UK Trader Scheme? |', function() {
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=NI&import_date=2021-01-01&import_destination=XI');
+  });
+  it('6.GB-XI | Are you authorised under the UK Trader Scheme? |', function() {
+    cy.visit('/duty-calculator/prefill?commodity_code=0702000007&country_of_origin=GB&import_date=2021-01-01&import_destination=XI');
   });
 });
