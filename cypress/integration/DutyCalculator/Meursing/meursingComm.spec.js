984d9eb54ec46baf297c1a16744f79b4905cca96
Madhu Dola <madviv@hotmail.com>
2021-10-04 17:52:45 +0100

    meursing code specs

---
+++ cypress/integration/DutyCalculator/Meursing/meursingComm.spec.js
@@ -0,0 +1,45 @@
+/* eslint-disable max-len */
+describe('| meursingComm | Store Commodity value in session for Meursing , Meursing on Comm Page', function() {
+  it('| meursing - Store Commodity code + Meursing code in Session |', function() {
+    const commcode = ['1905311100', '1704906500', '1704909991'];
+    for (let i=0; i<commcode.length; i++) {
+      cy.visit(`/xi/commodities/${commcode[i]}`);
+      cy.contains('Tools').click();
+      cy.contains('Meursing code finder').click();
+      cy.get('a[role=\'button\']').click();
+      cy.contains('0 - 4.99').click();
+      cy.contains('Continue').click();
+      cy.contains('5 - 29.99').click();
+      cy.contains('Continue').click();
+      cy.contains('1.5 - 2.99').click();
+      cy.contains('Continue').click();
+      cy.contains('2.5 - 5.99').click();
+      cy.contains('Continue').click();
+      cy.contains('Check your answers');
+      cy.contains('Continue').click();
+      cy.contains(`The Meursing additional code for a product with this composition is 7121.`);
+      cy.contains(`Return to ${commcode[i]}`).click();
+      // cy.get('.govuk-list.govuk-list--bullet > li:nth-of-type(1) > a').click();
+      cy.contains(`Commodity information for ${commcode[i]}`);
+      cy.get('a#tab_import').click();
+      cy.get('input#meursing-lookup-result-meursing-additional-code-id-field').should('have.value', '121');
+      cy.contains('Clear additional code').click();
+      cy.get('input#meursing-lookup-result-meursing-additional-code-id-field').should('not.have.value', '121');
+    }
+  });
+  it.only('| meursing calculator link on commodity import section |', function() {
+    cy.visit('xi/commodities/1905311100#import');
+    cy.contains('Enter a \'Meursing code\' to work out applicable duties');
+    cy.contains('This commodity code features duties which may be dependent on the sugar, flour, milk fat and milk protein content. To fully define the applicable duties, you need to specify the additional code that defines the content of these ingredients.');
+    cy.contains('Enter the 3-digit additional code');
+    cy.contains('If you know the additional code for your commodity, enter it in the box below. If you do not know the code, then use the Meursing code finder to find the additional code. You will be able to return to this page once you have found the right Meursing additional code.');
+    cy.get('span#meursing-lookup-result-meursing-additional-code-id-hint > .govuk-link').click();
+    cy.contains('Look up a Meursing code');
+    cy.go(-1);
+    cy.contains('Commodity information for 1905311100');
+    cy.get('input#meursing-lookup-result-meursing-additional-code-id-field').type('000');
+    cy.contains('Save code and update duties').click();
+
+    // add validations for calculations with /without code 
+  });
+});
--- cypress/integration/DutyCalculator/dcShared/dcExchangeRate.spec.js
+++ cypress/integration/DutyCalculator/dcShared/dcExchangeRate.spec.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-unused-vars */
 describe('💷 💶 | dcExchangeRate | Validating exchange rates |', function() {
   it('Exchange rate', function() {
     // import date
     cy.visit('/duty-calculator/uk/7202118000/import-date');
-    //    cy.visit('/import-date?referred_service=uk&commodity_code=7202118000')
     cy.validDate();
     cy.contains('Continue').click();
     // destination
     cy.get('#steps-import-destination-import-destination-xi-field').check();
     cy.contains('Continue').click();
     // origin
-    //  cy.get('input#steps-country-of-origin-country-of-origin-gb-field').click()
     cy.get('input#steps-country-of-origin-country-of-origin-gb-field').click();
     cy.contains('Continue').click();
     // trader scheme
@@ -23,34 +22,8 @@ describe('💷 💶 | dcExchangeRate | Validating exchange rates |', function() 
     cy.get('input#steps-customs-value-shipping-cost-field').clear().type('455.7533');
     cy.get('input#steps-customs-value-insurance-cost-field').clear().type('4545.987654');
     cy.contains('Continue').click();
-
     // confirm
     cy.get('.govuk-button').click();
-
-    // duty page
-    cy.contains('Import duty calculation');
-    cy.contains('You are importing commodity');
-    cy.contains('from United Kingdom (excluding Northern Ireland) on');
-    cy.contains('31 December 2021');
-    cy.contains('7202 11 80 00').click();
-    cy.contains('Commodity information for 7202118000');
-    cy.go(-1);
-    // keys
-    cy.get('.govuk-details > .govuk-details__summary');
-    cy.contains('Details of your trade').click();
-    cy.get('.govuk-details__text');
-    cy.contains('Origin:');
-    cy.contains('Commodity:');
-    cy.contains('Import date:');
-    cy.contains('Valuation of import:');
-    // values
-
-    // information
-    //   cy.contains('Third-country duty will apply as there is no preferential agreement in place for the import of this commodity.')
-    cy.get('.govuk-table__row');
-    cy.contains('Data');
-    cy.contains('Calculation');
-    cy.contains('Value');
 
     // Exchange Rate
     cy.request({
@@ -58,10 +31,13 @@ describe('💷 💶 | dcExchangeRate | Validating exchange rates |', function() 
       url: `https://www.trade-tariff.service.gov.uk/xi/api/v2/monetary_exchange_rates/`,
     }).then((response) => {
       expect(response.status).to.eq(200);
-      //   console.log(JSON.stringify(response.body))
-      const exchangerate = response.body.data[66].attributes.exchange_rate;
-      const m = parseFloat(exchangerate).toFixed(3);
+      // console.log(JSON.stringify(response.body))
+      const exchangerate = response.body.data[69].attributes.exchange_rate;
+      const m = parseFloat(exchangerate).toFixed(4);
       //   let n = exchangerate.toFixed(3)
+      // Updated on 1st October - change to dynamic
+      const startdate = response.body.data[69].attributes.validity_start_date;
+      console.log(startdate);
       console.log(m);
       cy.contains(m);
       cy.contains(`Please note - the current page uses an exchange rate of`);
