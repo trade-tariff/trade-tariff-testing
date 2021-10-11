e8e039d964a44c899a460a5c5d05a5f03b2fea6c
Madhu Dola <madviv@hotmail.com>
2021-10-11 17:28:15 +0100

    copy changes meursing

--- cypress/integration/HOTT-Shared/test.spec.js
+++ cypress/integration/HOTT-Shared/test.spec.js
@@ -4,7 +4,6 @@ describe.skip('🚀 | test.spec.js | Front end - mainpage |', function() {
   });
 
   it('🚀 UK 🇬🇧 - Main Page Validation', function() {
-    // Main Page
     cy.getExchangeRateForImportDate('2021-11-01').then(
         (exchangeRate) => console.log(exchangeRate.attributes.exchange_rate),
     );
