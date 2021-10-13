78e677434cee96b63d37f15db211c501c61d3929
Madhu Dola <madviv@hotmail.com>
2021-10-13 17:50:47 +0100

    import measure groups ordering

--- cypress/integration/HOTT-Shared/importMeasureGroups.spec.js
+++ cypress/integration/HOTT-Shared/importMeasureGroups.spec.js
@@ -1,7 +1,13 @@
-describe('UK 🇬🇧 XI 🇪🇺 | measureGrouping- UK & XI | validate if measures are grouped and clickable |', function() {
+describe('UK 🇬🇧 XI 🇪🇺 | importMeasureGrouping - UK & XI | validate if measures are grouped and clickable |', function() {
   // Main Page
   it('Northern Ireland - VAT and Excise , Custom Duties ,Trade remedies ,EU import controls ,UK import controls', function() {
     cy.visit('/xi/commodities/0304829010#import');
+    cy.get('li:nth-of-type(1) > .govuk-link').contains('Customs duties').click();
+    cy.get('li:nth-of-type(2) > .govuk-link').contains('Trade Remedies, safeguards and retaliatory duties').click();
+    cy.get('li:nth-of-type(3) > .govuk-link').contains('VAT and excise').click();
+    cy.get('li:nth-of-type(4) > .govuk-link').contains('EU import controls').click();
+    cy.get('li:nth-of-type(5) > .govuk-link').contains('UK import controls').click();
+
     cy.contains('VAT and excise').click();
     cy.contains('Customs duties').click();
     cy.contains('Trade Remedies, safeguards and retaliatory duties').click();
@@ -10,10 +16,10 @@ describe('UK 🇬🇧 XI 🇪🇺 | measureGrouping- UK & XI | validate if measu
   });
   it('United Kingdom - VAT and Excise , Custom Duties ,Trade remedies ,Quotas , import controls', function() {
     cy.visit('/commodities/0304829010#import');
-    cy.contains('VAT and excise').click();
-    cy.contains('Customs duties').click();
-    cy.contains('Trade Remedies, safeguards and retaliatory duties').click();
-    cy.contains('Import controls').click();
-    cy.contains('Quotas').click();
+    cy.get('li:nth-of-type(1) > .govuk-link').contains('Customs duties').click();
+    cy.get('li:nth-of-type(2) > .govuk-link').contains('Quotas').click();
+    cy.get('li:nth-of-type(3) > .govuk-link').contains('Trade Remedies, safeguards and retaliatory duties').click();
+    cy.get('li:nth-of-type(4) > .govuk-link').contains('VAT and excise').click();
+    cy.get('li:nth-of-type(5) > .govuk-link').contains('Import controls').click();
   });
 });
