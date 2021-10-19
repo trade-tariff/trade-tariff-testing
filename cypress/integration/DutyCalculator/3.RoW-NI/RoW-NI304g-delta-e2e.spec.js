01cb97a87515e40d23a1d0adb5f683b92035a9e1
Madhu Dola <madviv@hotmail.com>
2021-10-19 14:50:00 +0100

    RoW-XI non delta - EU route

--- cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI304g-delta-e2e.spec.js
+++ cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI304g-delta-e2e.spec.js
@@ -1,112 +1,110 @@
-//Quotas in delta route
-//1701141000 Qty 1 = UK , Qty 100 = EU Canada , Quotas associated 
-//Brazil , UK 
-describe('| Row-NI304g-delta.spec.js | 🔼 Delta Route | Quotas UK | ', function () {
-    it('RoW 🇨🇦 (Canada) - XI | UK Tariffs apply with Quotas |', function () {
-        cy.visit('/duty-calculator/xi/1701141000/import-date')
-        //date
-        cy.validDate()
-        //destination
-        cy.selectDestination('xi')
-        //origin
-        cy.selectOrigin('other')
-        //select country from list 
-        cy.wait(100)
-        cy.otherOriginList({ value: 'Canada' })
-        cy.wait(100)
-        //Trader Scheme
-        cy.traderScheme('yes')
-        // ✅  Final use in NI - Yes 
-        cy.finalUseNI('yes')
-        //Planned processing - acceptable3 
-        cy.plannedXI('acceptable3')
-        //customs value
-        cy.customsValue({ monetary: '500.00', shipping: '250.00', cost: '250.00' })
+// Quotas in delta route
+// 1701141000 Qty 1 = UK , Qty 100 = EU Canada , Quotas associated
+// Brazil , UK
+describe('| Row-NI304g-delta.spec.js | 🔼 Delta Route | Quotas UK | ', function() {
+  it('RoW 🇨🇦 (Canada) - XI | UK Tariffs apply with Quotas |', function() {
+    cy.visit('/duty-calculator/xi/1701141000/import-date');
+    // date
+    cy.validDate();
+    // destination
+    cy.selectDestination('xi');
+    // origin
+    cy.selectOrigin('other');
+    // select country from list
+    cy.wait(100);
+    cy.otherOriginList({value: 'Canada'});
+    cy.wait(100);
+    // Trader Scheme
+    cy.traderScheme('yes');
+    // ✅  Final use in NI - Yes
+    cy.finalUseNI('yes');
+    // Planned processing - acceptable3
+    cy.plannedXI('acceptable3');
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '250.00', cost: '250.00'});
 
-        //Import Quantity 
-        cy.quantity({ dtnr: '1', tne: '1', dap: '1', dtn: '1' })
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        cy.confirmPage()
-        cy.dutyPage()
-        cy.contains('Option 1: Third-country duty')
-        cy.contains('UK import duties apply, as the difference between the UK third country duty and the EU third country duty is lower than 3% of the customs value of your trade.')
-        cy.contains('Tariff preference (UK)')
+    // Import Quantity
+    cy.quantity({dtnr: '1', tne: '1', dap: '1', dtn: '1'});
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('UK import duties apply, as the difference between the UK third country duty and the EU third country duty is lower than 3% of the customs value of your trade.');
+    cy.contains('Tariff preference (UK)');
 
-        cy.contains('Option 2: Tariff preference - Canada')
-        cy.contains('UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.')
+    cy.contains('Option 2: Tariff preference - Canada');
+    cy.contains('UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.');
 
-        cy.contains('Option 3: Tariff preference - Canada')
-        cy.contains('UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.')
+    cy.contains('Option 3: Tariff preference - Canada');
+    cy.contains('UK preferential duties may be applied, as the difference between the UK preferential duty and the EU preferential duty is lower than 3% of the customs value of your trade.');
 
-        cy.contains('Option 4: Non-preferential quota 057713')
-        cy.contains('UK quotas may be used, as the difference between the UK in-quota duty and the EU third-country duty is lower than 3% of the customs value of your trade.')
-        cy.contains('Non Preferential Quota (UK)')
+    cy.contains('Option 4: Non-preferential quota 057713');
+    cy.contains('UK quotas may be used, as the difference between the UK in-quota duty and the EU third-country duty is lower than 3% of the customs value of your trade.');
+    cy.contains('Non Preferential Quota (UK)');
+  });
 
-    })
+  it('RoW 🇦🇺 (Australia) - XI | EU Tariffs apply without Quotas |', function() {
+    cy.visit('/duty-calculator/xi/1701141000/import-date');
+    // date
+    cy.validDate();
+    // destination
+    cy.selectDestination('xi');
+    // origin
+    cy.selectOrigin('other');
+    // select country from list
+    cy.wait(100);
+    cy.otherOriginList({value: 'Australia'});
+    cy.wait(100);
+    // Trader Scheme
+    cy.traderScheme('yes');
+    // ✅  Final use in NI - Yes
+    cy.finalUseNI('yes');
+    // Planned processing - acceptable3
+    cy.plannedXI('acceptable3');
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '250.00', cost: '250.00'});
 
-    it('RoW 🇦🇺 (Australia) - XI | EU Tariffs apply without Quotas |', function () {
-        cy.visit('/duty-calculator/xi/1701141000/import-date')
-        //date
-        cy.validDate()
-        //destination
-        cy.selectDestination('xi')
-        //origin
-        cy.selectOrigin('other')
-        //select country from list 
-        cy.wait(100)
-        cy.otherOriginList({ value: 'Australia' })
-        cy.wait(100)
-        //Trader Scheme
-        cy.traderScheme('yes')
-        // ✅  Final use in NI - Yes 
-        cy.finalUseNI('yes')
-        //Planned processing - acceptable3 
-        cy.plannedXI('acceptable3')
-        //customs value
-        cy.customsValue({ monetary: '500.00', shipping: '250.00', cost: '250.00' })
+    // Import Quantity
+    cy.quantity({dtnr: '1', tne: '1', dap: '1'});
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
 
-        //Import Quantity 
-        cy.quantity({ dtnr: '1', tne: '1', dap: '1' })
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        
-        cy.confirmPage()
-        cy.dutyPage()
-        cy.contains('Option 1: Third-country duty')
-        cy.contains('UK import duties apply, as the difference between the UK third country duty and the EU third country duty is lower than 3% of the customs value of your trade.')
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('UK import duties apply, as the difference between the UK third country duty and the EU third country duty is lower than 3% of the customs value of your trade.');
 
-        cy.contains('Option 2: Non-preferential quota 057713')
-        cy.contains('UK quotas may be used, as the difference between the UK in-quota duty and the EU third-country duty is lower than 3% of the customs value of your trade.')
-        cy.contains('Non Preferential Quota (UK)')
+    cy.contains('Option 2: Non-preferential quota 057713');
+    cy.contains('UK quotas may be used, as the difference between the UK in-quota duty and the EU third-country duty is lower than 3% of the customs value of your trade.');
+    cy.contains('Non Preferential Quota (UK)');
 
-        //EU Tariffs - Change Quantity to override Quotas 
-        cy.get('.govuk-back-link').click()
-        cy.get('div:nth-of-type(10) > .govuk-summary-list__actions > .govuk-link').click()
-        //Import Quantity 
-        cy.quantity({ dtnr: '100', tne: '100', dap: '100' })
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        //doc code
-        cy.docCode({ uk: 'n990' })
-        cy.contains('Continue').click()
-        cy.confirmPage()
-        cy.dutyPage()
-        cy.contains('Option 1: Third-country duty')
-        cy.contains('Third-country duty (EU)')
-        cy.contains('EU import duties apply, as the difference between the UK third country duty and the EU third country duty exceeds 3% of the customs value of your trade.')
-    })
-
-})
+    // EU Tariffs - Change Quantity to override Quotas
+    cy.get('.govuk-back-link').click();
+    cy.get('div:nth-of-type(10) > .govuk-summary-list__actions > .govuk-link').click();
+    // Import Quantity
+    cy.quantity({dtnr: '100', tne: '100', dap: '100'});
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    // doc code
+    cy.docCode({uk: 'n990'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Third-country duty (EU)');
+    cy.contains('EU import duties apply, as the difference between the UK third country duty and the EU third country duty exceeds 3% of the customs value of your trade.');
+  });
+});
--- cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI306-e2e.spec.js
+++ cypress/integration/DutyCalculator/3.RoW-NI/RoW-NI306-e2e.spec.js
@@ -1,31 +1,104 @@
-// 🚫 Trade Remedies - 🚫 0% MFN EU tariff - Trader Scheme - 🚫 UK Trader Scheme 
-describe('| RoW-NI306-e2e.spec | RoW (Greenland) to Northern Ireland ', function () {
-    //
-    it('RoW 🇬🇱 to NI', function () {
-        cy.visit('/duty-calculator/uk/0702000007/import-date')
-        //date
-        cy.validDate()
-        //destination
-        cy.selectDestination('xi')
-        //origin
-        cy.selectOrigin('other')
-        //select country from list 
-        cy.wait(700)
-        cy.otherOriginList({ value: 'Greenland' })
-        cy.wait(700)
-        //Trader Scheme
-        cy.traderScheme('no')
-        //Duties Apply 
-        cy.euDutiesApply()
-        //customs value
-        cy.customsValue({ monetary: '500.00', shipping: '100.00', cost: '250.00' })
-        //Import Quantity 
-        cy.quantity({ dtn: '230.98' })
-        cy.confirmPage()
-        cy.dutyPage()
-        cy.contains('Option 1: Third-country duty')
-        cy.contains('Option 2: Tariff preference - OCTs (Overseas Countries and Territories)')
-        cy.contains('Third-country duty (EU)')
+// 🚫 Trade Remedies - 🚫 0% MFN EU tariff - 🚫 UK Trader Scheme
+describe('| RoW-NI306-e2e.spec | RoW (Greenland) to Northern Ireland ', function() {
+  //
+  it('RoW 🇬🇱 to NI', function() {
+    cy.visit('/duty-calculator/uk/0702000007/import-date');
+    // date
+    cy.validDate();
+    // destination
+    cy.selectDestination('xi');
+    // origin
+    cy.selectOrigin('other');
+    // select country from list
+    cy.wait(700);
+    cy.otherOriginList({value: 'Greenland'});
+    cy.wait(700);
+    // Trader Scheme
+    cy.traderScheme('no');
+    // Duties Apply
+    cy.euDutiesApply();
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
+    // Import Quantity
+    cy.quantity({dtn: '230.98'});
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Option 2: Tariff preference - OCTs (Overseas Countries and Territories)');
+    cy.contains('Third-country duty (EU)');
+  });
+  it('RoW 🇨🇳 China to XI', function() {
+    cy.visit('/duty-calculator/uk/8421999099/import-date');
+    // date
+    cy.validDate();
+    // destination
+    cy.selectDestination('xi');
+    // origin
+    cy.selectOrigin('other');
+    // select country from list
+    cy.wait(500);
+    cy.otherOriginList({value: 'China'});
+    cy.wait(500);
+    // Trader Scheme
+    cy.traderScheme('no');
+    // Duties Apply
+    cy.euDutiesApply();
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
+    // Import Quantity
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Third-country duty (EU)');
 
-    })
-})
+    // change options
+    cy.get('.govuk-back-link').click();
+    cy.get('div:nth-of-type(6) > .govuk-summary-list__actions > .govuk-link').click();
+    // Trader Scheme
+    cy.traderScheme('yes');
+    // ✅  Final use in NI - Yes
+    cy.finalUseNI('no');
+    // Planned processing - commercial
+    //  cy.plannedXI('commercial');
+    // Duties Apply
+    cy.euDutiesApply();
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
+    // Import Quantity
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Third-country duty (EU)');
+
+    // change options
+    cy.get('.govuk-back-link').click();
+    cy.get('div:nth-of-type(6) > .govuk-summary-list__actions > .govuk-link').click();
+    // Trader Scheme
+    cy.traderScheme('yes');
+    // ✅  Final use in NI - Yes
+    cy.finalUseNI('yes');
+    // Planned processing - commercial
+    cy.plannedXI('commercial');
+    // Duties Apply
+    cy.euDutiesApply();
+    // customs value
+    cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
+    // Import Quantity
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.docCode({xi: 'none'});
+    cy.contains('Continue').click();
+    cy.confirmPage();
+    cy.dutyPage();
+    cy.contains('Option 1: Third-country duty');
+    cy.contains('Third-country duty (EU)');
+  });
+});
