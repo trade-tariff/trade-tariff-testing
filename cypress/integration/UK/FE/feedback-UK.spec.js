7e3c18df0f6845e5f2b2836af7226831f6eed60f
Madhu Dola <madviv@hotmail.com>
2021-10-25 11:21:21 +0100

    modify contact us link on feedback page

--- cypress/integration/UK/FE/feedback-UK.spec.js
+++ cypress/integration/UK/FE/feedback-UK.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-describe.skip('🇬🇧 💡 | feedback-UK | feedback link is available and user is able to send feedback |', function() {
+describe('🇬🇧 💡 | feedback-UK | feedback link is available and user is able to send feedback |', function() {
   it('UK - All pages- Feedback link available  ', function() {
     const pages = ['/sections/1', '/chapters/01', '/headings/0101', '/commodities/0101210000'];
     for (let i = 0; i < pages.length; i++) {
@@ -17,7 +17,7 @@ describe.skip('🇬🇧 💡 | feedback-UK | feedback link is available and user
     cy.contains('Send your feedback');
     cy.contains('Don\'t include any personal information. This form is for submitting feedback on the website. If you have a question related to classifying a good then please contact HMRC. If you have a complaint about the phone service then please see this page.');
     // first link
-    cy.get('.form-hint.govuk-hint > a:nth-of-type(1)').should('have.attr', 'href', 'https://www.gov.uk/government/organisations/hm-revenue-customs/contact/vat-enquiries');
+    cy.get('.form-hint.govuk-hint > a:nth-of-type(1)').should('have.attr', 'href', 'https://www.gov.uk/guidance/ask-hmrc-for-advice-on-classifying-your-goods');
     // second link
     cy.get('.form-hint.govuk-hint > a:nth-of-type(2)').should('have.attr', 'href', 'https://www.gov.uk/guidance/complain-to-hm-revenue-and-customs');
 
@@ -26,12 +26,12 @@ describe.skip('🇬🇧 💡 | feedback-UK | feedback link is available and user
     cy.get('.error-message');
     cy.contains('can\'t be blank');
 
-    // enter some feedback
-    cy.get('.govuk-textarea').type(' 👨🏻‍💻 Cypress Test - 🇬🇧 🇪🇺  feedback ');
-    cy.get('input#name').type('Random Guy 🥸 ');
-    cy.get('input#email').type('abd@12398.com');
-    cy.get('input[name=\'commit\']').click();
-    //  cy.contains('Thank you for your feedback')
+    // // enter some feedback
+    // cy.get('.govuk-textarea').type(' 👨🏻‍💻 Cypress Test - 🇬🇧 🇪🇺  feedback ');
+    // cy.get('input#name').type('Random Guy 🥸 ');
+    // cy.get('input#email').type('abd@12398.com');
+    // cy.get('input[name=\'commit\']').click();
+    // //  cy.contains('Thank you for your feedback')
   });
   it('UK - The UK has left the EU', function() {
     cy.visit('/sections');
