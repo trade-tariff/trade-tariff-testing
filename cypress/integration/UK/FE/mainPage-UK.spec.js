5782575aeacb900df6f846d3504fd364ef006354
Madhu Dola <madviv@hotmail.com>
2021-11-22 14:03:49 +0000

    Context Selector added to Chapters ,Headings and Commodities

--- cypress/integration/UK/FE/mainPage-UK.spec.js
+++ cypress/integration/UK/FE/mainPage-UK.spec.js
@@ -12,9 +12,25 @@ describe('🇬🇧 💡 | mainPage-UK | Main Page - headers ,sections  - (UK ver
     cy.visit('/sections');
     cy.get('.govuk-header').should('be.visible', 'GOV.UK');
   });
-  it('UK - Sections details on heading ', function() {
+  it('UK - find_commodity page', function() {
+    cy.visit('/find_commodity');
+    cy.contains('The UK Integrated Online Tariff');
+    cy.contains('Look up commodity codes, import duties, taxes and controls');
+    cy.contains('Are you importing goods into Northern Ireland?');
+    //  cy.contains('If you\'re bringing goods into Northern Ireland from outside the UK and the EU, you will pay the UK duty rate if your goods are not \'at risk\' of onward movement to the EU. If they are at risk of onward movement to the EU, use the Northern Ireland Online Tariff.');
+    cy.contains('Search for a commodity');
+    cy.contains('The UK Integrated Online Tariff helps you to:');
+    cy.contains('find commodity codes for imports into or exports out of the UK');
+    cy.contains('find the taxes and duties that apply to those imports');
+    cy.contains('find out which certificates and licences apply to the import of your goods');
+    cy.contains('The first step is to find the commodity that you are importing or exporting.');
+    cy.contains('Enter the name of the goods or a commodity code');
+    cy.contains('Commodity codes are 10-digit numbers that classify goods so you can fill in declarations and other paperwork. You must use the right commodity code.');
+    cy.contains('Alternatively, you can browse the goods classification or look for your product in the A-Z.');
+  });
+  it('UK - Browse page ', function() {
     cy.visit('/browse');
-    cy.contains('Browse the tariff');
+    cy.browsePage();
     cy.contains(' Live animals; animal products').click();
     cy.contains('Section I - Live animals; animal products');
     cy.contains('Section I contains 5 chapters. Choose the chapter that best matches your goods.');
@@ -26,15 +42,51 @@ describe('🇬🇧 💡 | mainPage-UK | Main Page - headers ,sections  - (UK ver
     cy.get('.govuk-breadcrumbs__link').contains('Home').click();
     cy.contains('Look up commodity codes, import duties, taxes and controls');
   });
-  it('UK - Heading 2902', function() {
-    cy.visit('/headings/2902');
-    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+  it('UK - Sections page ', function() {
+    cy.visit('/sections/12');
+    cy.contains('UK Integrated Online Tariff');
+    cy.contains('Section XII - Footwear, headgear, umbrellas, sun umbrellas, walking-sticks, seat-sticks, whips, riding-crops and parts thereof; prepared feathers and articles made therewith; artificial flowers; articles of human hair');
+    cy.contains('Section XII contains 4 chapters. Choose the chapter that best matches your goods.');
   });
-  it('UK - Chapter 99 ', function() {
+  it('UK - Chapter page ', function() {
     cy.visit('/chapters/99');
     cy.contains('Choose the heading that best matches your goods');
+    cy.get('.govuk-summary-list').contains('Chapter');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+
     cy.contains('Goods delivered to vessels and aircraft').click();
     cy.contains('Choose the commodity code below that best matches your goods to see more information');
+  });
+  it('UK - Heading page', function() {
+    cy.visit('/headings/2902');
+    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+    cy.get('.govuk-summary-list').contains('Heading');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+  });
+  it('UK - Commodity page - without Supp units', function() {
+    cy.visit('/commodities/6406109010');
+    cy.get('.govuk-summary-list').contains('Commodity');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.contains('Parts of footwear (including uppers whether or not attached to soles other than outer soles); removable insoles, heel cushions and similar articles; gaiters, leggings and similar articles, and parts thereof — Uppers and parts thereof, other than stiffeners — Of other materials — Hand-made');
+    cy.get('.govuk-summary-list').contains('Supplementary unit');
+    cy.get('.govuk-summary-list').contains('No supplementary unit required.');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.get('.govuk-summary-list').contains('What are supplementary units?').click();
+    cy.contains('Supplementary units are used when an additional measurement unit is needed on customs declarations. For example: the quantity of the products as well as the weight in kilograms.');
+  });
+  it('UK - Commodity page - with Supp units', function() {
+    cy.visit('/commodities/6401929000');
+    cy.get('.govuk-summary-list').contains('Commodity');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.contains('Waterproof footwear with outer soles and uppers of rubber or of plastics, the uppers of which are neither fixed to the sole nor assembled by stitching, riveting, nailing, screwing, plugging or similar processes — Other footwear — Covering the ankle but not covering the knee — With uppers of plastics');
+    cy.get('.govuk-summary-list').contains('Supplementary unit');
+    cy.get('.govuk-summary-list').contains('Number of pairs (pa)');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.get('.govuk-summary-list').contains('What are supplementary units?').click();
+    cy.contains('Supplementary units are used when an additional measurement unit is needed on customs declarations. For example: the quantity of the products as well as the weight in kilograms.');
   });
   it('UK - Search the Tariff section', function() {
     cy.visit('/sections');
--- cypress/integration/XI/FE/mainPage-XI.spec.js
+++ cypress/integration/XI/FE/mainPage-XI.spec.js
@@ -11,9 +11,25 @@ describe('🇪🇺 💡 | mainPage-XI | Main Page ,headings ,sections - (XI vers
     cy.visit('/xi/sections');
     cy.get('.govuk-header').should('be.visible', 'GOV.UK');
   });
-  it('XI - Sections details on heading ', function() {
+  it('XI - find_commodity page', function() {
+    cy.visit('xi/find_commodity');
+    cy.contains('Northern Ireland Online Tariff');
+    cy.contains('Look up commodity codes, import duties, taxes and controls');
+    cy.contains('Are your goods at risk of onward movement to the EU?');
+    //  cy.contains('If you\'re bringing goods into Northern Ireland from outside the UK and the EU, you will pay the UK duty rate if your goods are not \'at risk\' of onward movement to the EU. If they are at risk of onward movement to the EU, use the Northern Ireland Online Tariff.');
+    cy.contains('Search for a commodity');
+    cy.contains('The Northern Ireland Online Tariff helps you to:');
+    cy.contains('find commodity codes for imports into or exports out of Northern Ireland');
+    cy.contains('find the taxes and duties that apply to those imports');
+    cy.contains('find out which certificates and licences apply to the import of your goods');
+    cy.contains('The first step is to find the commodity that you are importing or exporting.');
+    cy.contains('Enter the name of the goods or a commodity code');
+    cy.contains('Commodity codes are 10-digit numbers that classify goods so you can fill in declarations and other paperwork. You must use the right commodity code.')
+    cy.contains('Alternatively, you can browse the goods classification or look for your product in the A-Z.');
+  });
+  it('XI - Browse page ', function() {
     cy.visit('xi/browse');
-    cy.contains('Browse the tariff');
+    cy.browsePage();
     cy.contains(' Live animals; animal products').click();
     cy.contains('Section I - Live animals; animal products');
     cy.contains('Section I contains 5 chapters. Choose the chapter that best matches your goods.');
@@ -25,16 +41,53 @@ describe('🇪🇺 💡 | mainPage-XI | Main Page ,headings ,sections - (XI vers
     cy.get('.govuk-breadcrumbs__link').contains('Home').click();
     cy.contains('Look up commodity codes, import duties, taxes and controls');
   });
-  it('XI - Heading 2902', function() {
-    cy.visit('/xi/headings/2902');
-    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+   it('XI - Sections page ', function() {
+    cy.visit('xi/sections/12');
+    cy.contains('UK Integrated Online Tariff');
+    cy.contains('Section XII - Footwear, headgear, umbrellas, sun umbrellas, walking-sticks, seat-sticks, whips, riding-crops and parts thereof; prepared feathers and articles made therewith; artificial flowers; articles of human hair');
+    cy.contains('Section XII contains 4 chapters. Choose the chapter that best matches your goods.');
   });
-  it('XI - Chapter 99 ', function() {
-    cy.visit('/xi/chapters/99');
+  it('XI - Chapter page ', function() {
+    cy.visit('xi/chapters/99');
     cy.contains('Choose the heading that best matches your goods');
+    cy.get('.govuk-summary-list').contains('Chapter');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+
     cy.contains('Goods delivered to vessels and aircraft').click();
     cy.contains('Choose the commodity code below that best matches your goods to see more information');
   });
+  it('XI - Heading page', function() {
+    cy.visit('xi/headings/2902');
+    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+    cy.get('.govuk-summary-list').contains('Heading');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.contains('Choose the commodity code below that best matches your goods to see more information');
+  });
+  it('XI - Commodity page - without Supp units', function() {
+    cy.visit('xi/commodities/6406109010');
+    cy.get('.govuk-summary-list').contains('Commodity');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.contains('Parts of footwear (including uppers whether or not attached to soles other than outer soles); removable insoles, heel cushions and similar articles; gaiters, leggings and similar articles, and parts thereof — Uppers and parts thereof, other than stiffeners — Of other materials — Hand-made');
+    cy.get('.govuk-summary-list').contains('Supplementary unit');
+    cy.get('.govuk-summary-list').contains('No supplementary unit required.');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.get('.govuk-summary-list').contains('What are supplementary units?').click();
+    cy.contains('Supplementary units are used when an additional measurement unit is needed on customs declarations. For example: the quantity of the products as well as the weight in kilograms.');
+  });
+  it('XI - Commodity page - with Supp units', function() {
+    cy.visit('xi/commodities/6401929000');
+    cy.get('.govuk-summary-list').contains('Commodity');
+    cy.get('.govuk-summary-list').contains('Classification');
+    cy.contains('Waterproof footwear with outer soles and uppers of rubber or of plastics, the uppers of which are neither fixed to the sole nor assembled by stitching, riveting, nailing, screwing, plugging or similar processes — Other footwear — Covering the ankle but not covering the knee — With uppers of plastics');
+    cy.get('.govuk-summary-list').contains('Supplementary unit');
+    cy.get('.govuk-summary-list').contains('Number of pairs (pa)');
+    cy.get('.govuk-summary-list').contains('Date of trade');
+    cy.get('.govuk-summary-list').contains('What are supplementary units?').click();
+    cy.contains('Supplementary units are used when an additional measurement unit is needed on customs declarations. For example: the quantity of the products as well as the weight in kilograms.');
+  });
+ 
   it('XI - Search the Tariff section', function() {
     cy.visit('/xi/sections');
     cy.contains('Search the Northern Ireland Online Tariff');
