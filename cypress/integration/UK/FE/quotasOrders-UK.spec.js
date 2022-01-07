58088dcf24faef8acbb7ee41eab1fc156fcb898d
Madhu Dola <madviv@hotmail.com>
2022-01-07 04:26:08 +0000

    HOTT-1163 Quotas text block

--- cypress/integration/UK/FE/quotasOrders-UK.spec.js
+++ cypress/integration/UK/FE/quotasOrders-UK.spec.js
@@ -4,6 +4,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/0201100021#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Non preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('054002').click();
     cy.get('.tariff-info')
@@ -15,6 +17,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/0204501100#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Non preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('052016').click();
     cy.get('.tariff-info')
@@ -25,6 +29,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/0201100021#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Non preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('058400').click();
     cy.get('.tariff-info')
@@ -36,6 +42,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/2202999910#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('056721').click();
     cy.get('.tariff-info').focus()
@@ -57,6 +65,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/2302101000#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('056711').click();
     cy.get('.tariff-info').focus()
@@ -77,6 +87,8 @@ describe('🇬🇧 💡 | quotasOrders-UK | Check specific quota Quotadetails |'
     cy.visit('/commodities/1604207094#import');
     cy.get('.govuk-tabs__panel');
     cy.contains('Preferential tariff quota');
+    cy.contains('Quotas allow a limited amount of goods to be imported at a lower duty rate. Once the quota has been used up, you will revert to the out-of-quota rate, which may be the third-country duty or a preferential rate.');
+    cy.contains('Quotas can be non-preferential (open to many countries) or preferential (restricted to a particular trading partner).');
     cy.get('.table-line');
     cy.contains('051618').click();
     cy.get('.tariff-info').focus()
