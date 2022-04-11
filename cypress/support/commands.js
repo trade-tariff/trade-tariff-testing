9cd0896c517f4303e90ef1b588a8fdde9ea4bb57
Madhu Dola <madviv@hotmail.com>
2022-04-11 13:04:04 +0100

    HOTT-1464 Guidance notes on commodity page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -196,9 +196,16 @@ Cypress.Commands.add('code999L', ()=>{
   cy.get('.info-content').contains('Customs Declaration Service (CDS) universal waiver');
   cy.get('.info-content').contains('Requirement for a licence is waived by entering the 999L document code and the document identifier CDS WAIVER in the additional documentation field for this commodity item. 999L can be used for CDS in a similar way to LIC99 on the CHIEF system, when a waiver may be applied.');
 });
-Cypress.Commands.add('certificateGuidanceUK', ()=>{
+Cypress.Commands.add('certificateGuidance', ()=>{
   cy.contains('Using this certificate on CDS or CHIEF').click();
   cy.contains('CDS guidance:');
   cy.contains('CHIEF guidance:');
   cy.contains('Commodity codes that require this certificate').click();
+});
+Cypress.Commands.add('commodityGuidance', ()=>{
+  cy.get('.info-inner').contains('Guidance for completing Box 44 or Data Element 2/3').click();
+  cy.contains('CDS guidance');
+  cy.contains('CHIEF guidance');
+  cy.get('.info-inner').contains('document status codes: ');
+  cy.get('.info-inner').contains(' status codes');
 });
