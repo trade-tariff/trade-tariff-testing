511ec8bb760a951220b6803549ec0b7ededa8077
Madhu Dola <madviv@hotmail.com>
2022-04-11 17:40:21 +0100

    Regression fixes

--- cypress/integration/HOTT-Shared/documentCodeTitles.spec.js
+++ cypress/integration/HOTT-Shared/documentCodeTitles.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-describe('UK 🇬🇧 XI 🇪🇺 | documentCodeTitles.spec.js | Validate document / certificate description |', function() {
+describe('UK 🇬🇧 XI 🇪🇺 | documentCodeTitles.spec.js | Validate document / certificate description + validity period |', function() {
   // HOTT-1255 - remove certificate document title from pop up window
   it(`UK - Certificate / Document details pop up `, function() {
     cy.visit(`/commodities/0104103000`);
@@ -9,28 +9,29 @@ describe('UK 🇬🇧 XI 🇪🇺 | documentCodeTitles.spec.js | Validate docume
     cy.get('table:nth-of-type(1) > .govuk-table__body > tr:nth-of-type(1)  a[role=\'button\']').click();
     cy.contains('Other certificates:').should('not.exist');
     cy.contains('Particular provisions:').should('not.exist');
+    cy.get('.info-inner').contains('Import control - CITES for All countries');
+    cy.get('.info-inner').contains(' From 29 Jan 2022 to 30 Sep 2023');
     // div#popup a
-    cy.get('div#popup a').click().wait(500);
-    //
-    cy.get('tr:nth-of-type(2) > .conditions-col.govuk-table__cell > a[role=\'button\']').click();
-    cy.contains('National Document:').should('not.exist');
-    cy.get('div#popup a').click().wait(500);
-    //
-    cy.get('[data-module] [tabindex=\'-1\']:nth-of-type(3) .conditions-col [href]').click();
-    cy.contains('Other certificates:').should('not.exist');
+    cy.get('.close [href]').click();
   });
   it(`XI - Certificate / Document details pop up `, function() {
     cy.visit(`xi/commodities/9706100000`);
     cy.get('.govuk-breadcrumbs__list').contains('Commodity 9706100000');
     cy.checkCommPage('9706100000');
-    // //
-    cy.get('[data-module] .govuk-table:nth-child(9) [tabindex=\'-1\']:nth-of-type(1) .conditions-col [href]').click();
+    // cy.get('[data-module] .govuk-table:nth-child(9) [tabindex=\'-1\']:nth-of-type(1) .conditions-col [href]').click();
+    cy.get('#measure-3896733').contains('Conditions').click();
     cy.contains('Other certificates:').should('not.exist');
     cy.contains('Particular provisions:').should('not.exist');
-    cy.get('div#popup a').click().wait(500);
-    //
-    cy.get('table:nth-of-type(3) > .govuk-table__body > .KP.govuk-table__row > .conditions-col.govuk-table__cell > a[role=\'button\']').click();
+    cy.get('.info-inner').contains('Import control - CITES for All countries');
+    cy.contains('From 19 Jan 2022');
+    cy.get('.close [href]').click().wait(300);
+    cy.get('#measure-3617164').contains('Conditions').click().wait(250);
+    // cy.get('div#popup a').click().wait(500);
+    // //
+    // cy.get('table:nth-of-type(3) > .govuk-table__body > .KP.govuk-table__row > .conditions-col.govuk-table__cell > a[role=\'button\']').click();
     cy.contains('Particular provisions:').should('not.exist');
+    cy.contains('Import control on luxury goods for North Korea (Democratic People’s Republic of Korea)');
+    cy.get('.info-inner').contains('From 1 Feb 2018');
     cy.get('div#popup a').click().wait(500);
     cy.contains('Goods necessary for the official purposes of diplomatic or consular missions of Member States in the DPRK or international organisations enjoying immunities in accordance with international law, or to the personal effects of their staff (Art 10.3 of Regulation (EU) 2017/1509)');
   });
@@ -45,6 +46,6 @@ describe('UK 🇬🇧 XI 🇪🇺 | documentCodeTitles.spec.js | Validate docume
     cy.visit('xi/commodities/0702000007');
     cy.get('#measure-20164273').contains('Conditions').click();
     cy.get('.info-content').contains('Phytosanitary Certificate (import) for All countries');
-    cy.contains('999L').should('not.exist');
+    cy.get('.info-content').should('not.contain', '999L');
   });
 });
