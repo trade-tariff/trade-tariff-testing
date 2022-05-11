dffa6709a7d0e53a69bc48c90c48ef5d1d1135cf
William Fish <william.michael.fish@gmail.com>
2022-05-11 16:56:24 +0100

    Refactor the quota api specs slightly after data changes

--- cypress/support/apiCommands.js
+++ cypress/support/apiCommands.js
@@ -1,4 +1,3 @@
-
 // API Commands
 Cypress.Commands.add('validJsonAPIresponse', (response)=>{
   expect(response.status).to.eq(200);
@@ -6,4 +5,20 @@ Cypress.Commands.add('validJsonAPIresponse', (response)=>{
   expect(response.headers).to.include({'content-type': 'application/json; charset=utf-8'});
   expect(response.body).to.not.be.null;
   expect(response.body).to.have.property('data');
+});
+
+Cypress.Commands.add('hasMeasureType', (response, expectedId, expectedDescription) => {
+  const matchingMeasureType = response.body.included.filter(function(resource) {
+    return (resource.type === 'measure_type' && resource.id === expectedId);
+  })[0];
+
+  expect(matchingMeasureType.attributes.description).to.eq(expectedDescription);
+});
+
+Cypress.Commands.add('doesNotHaveMeasureType', (response, expectedId) => {
+  const matchingMeasureTypes = response.body.included.filter(function(resource) {
+    return (resource.type === 'measure_type' && resource.id === expectedId);
+  });
+
+  expect(matchingMeasureTypes).to.be.empty;
 });
