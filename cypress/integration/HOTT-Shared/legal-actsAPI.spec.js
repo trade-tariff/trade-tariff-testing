3426ecf493abb06659c0de0ec10d6f5a7585662d
M Dola <52670394+madviv@users.noreply.github.com>
2022-05-17 15:40:03 +0100

    Update legal-actsAPI.spec.js

--- cypress/integration/HOTT-Shared/legal-actsAPI.spec.js
+++ cypress/integration/HOTT-Shared/legal-actsAPI.spec.js
@@ -7,21 +7,6 @@ describe('| legal-actsAPI.spec.js |  | legal acts API checks  |', function() {
           cy.hasLegalActs(response, 'R9703380', 'http://eur-lex.europa.eu/search.html?whOJ=NO_OJ%3D61,YEAR_OJ%3D1997,PAGE_FIRST%3D0001&DB_COLL_OJ=oj-l&type=advanced&lang=en');
         });
   });
-  // it('XI - V2 API check  ', function() {
-  //   cy.request({
-  //     method: 'GET',
-  //     url: '/xi/api/v2/commodities/9403601000',
-  //   }).then((response) => {
-  //     expect(response.status).to.eq(200);
-  //     //   console.log(JSON.stringify(response.body))
-  //     expect(response.body).not.to.be.null;
-  //     expect(response.body.included[245].attributes).to.have.property('regulation_url')
-  //         .contains('http://eur-lex.europa.eu/search.html?whOJ=NO_OJ%3D292,YEAR_OJ%3D1998,PAGE_FIRST%3D0001&DB_COLL_OJ=oj-l&type=advanced&lang=en');
-  //     console.log(response.body.included[245]);
-  //     expect(response.body.included[245]).to.have.property('id')
-  //         .contains('R9822610');
-  //   });
-  // });
   it('XI - V2 API check ', function() {
     cy.request('xi/api/v2/commodities/9403601000')
         .then((response) => {
