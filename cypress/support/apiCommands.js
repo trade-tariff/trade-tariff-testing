a1528ddd59b7c5ec1296cb9cbe59030ccdf9a202
M Dola <52670394+madviv@users.noreply.github.com>
2022-05-17 15:48:30 +0100

    Update apiCommands.js

--- cypress/support/apiCommands.js
+++ cypress/support/apiCommands.js
@@ -23,11 +23,10 @@ Cypress.Commands.add('doesNotHaveMeasureType', (response, expectedId) => {
   expect(matchingMeasureTypes).to.be.empty;
 });
 Cypress.Commands.add('hasLegalActs', (response, expectedId, expectedDescription) => {
-  expect(response.status).to.eq(200);
-  expect(response.body).not.to.be.null;
   const matchingMeasureType = response.body.included.filter(function(resource) {
     return (resource.type === 'legal_act' && resource.id === expectedId);
   })[0];
-
+  expect(response.status).to.eq(200);
+  expect(response.body).not.to.be.null;
   expect(matchingMeasureType.attributes.regulation_url).to.eq(expectedDescription);
 });
