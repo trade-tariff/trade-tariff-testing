47848813572d3e4ee3e20c9aeecc7a2443faa496
Madhu Dola <madviv@hotmail.com>
2022-05-23 21:35:57 +0100

    Regression fixes

--- cypress/integration/XI/FE/pages-XI.spec.js
+++ cypress/integration/XI/FE/pages-XI.spec.js
@@ -55,7 +55,7 @@ describe('🇪🇺 💡 | pages-XI.spec.js | Main Page ,headings ,sections - (XI
     cy.get('.govuk-summary-list').contains('Date of trade');
     cy.contains('Chapter 98 contains 1 heading. Choose the heading that best matches your goods.');
 
-    cy.contains('Component parts of complete industrial plant in the framework of external trade (Commission Regulation EC No 113/2010 of 9.02.2010)').click();
+    cy.contains('Component parts of complete industrial plant in the framework of external trade (Commission Implementing Regulation (EU) 2020/1197 of 30 July 2020)').click();
     cy.contains('There are 96 commodities in this category. Choose the commodity code that best matches your goods to see more information. If your item is not listed by name, it may be shown under what it\'s used for, what it\'s made from or \'Other\'.');
   });
   it('XI - Heading page', function() {
