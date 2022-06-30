45fd61d3fcbb0099df119dec8d53279114fac4d8
William Fish <william.michael.fish@gmail.com>
2022-06-30 10:02:09 +0100

    Fixes issue with typescript and windows paths

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://staging.trade-tariff.service.gov.uk',
-
-    commcodes: [
+    'baseUrl': 'https://staging.trade-tariff.service.gov.uk',
+    'specPattern': ['**/*.spec.js', '**/*.feature'],
+    'commcodes': [
       '7202118000',
       '0201100021',
       '0406103010',
@@ -20,8 +20,8 @@ module.exports = defineConfig({
       '1704907100',
       '2206001000',
     ],
-    termcodes: ['0302910095', '0303541090', '0303919091'],
-    newcodes: [
+    'termcodes': ['0302910095', '0303541090', '0303919091'],
+    'newcodes': [
       '7318159511',
       '2921490045',
       '2921290070',
@@ -29,11 +29,10 @@ module.exports = defineConfig({
       '2918290040',
       '2916399016',
     ],
-    accessibility: true,
-    fixtures_timestamp: '2021-02-09',
-
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
+    'accessibility': true,
+    'fixtures_timestamp': '2021-02-09',
+    'reporter': 'cypress-multi-reporters',
+    'reporterOptions': {
       reporterEnabled: 'mochawesome',
       mochawesomeReporterOptions: {
         reportDir: 'cypress/reports/mocha',
@@ -43,22 +42,29 @@ module.exports = defineConfig({
         json: true,
       },
     },
-    chromeWebSecurity: false,
-    firefoxWebSecurity: false,
-    video: false,
-    screenshotOnRunFailure: false,
-    projectId: '7p655m',
-    parseSpecialCharSequences: false,
-    defaultCommandTimeout: 15000,
-    responseTimeout: 60000,
-    e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-      setupNodeEvents(on, config) {
-        return require('./cypress/plugins/index.js')(on, config);
-      },
-      baseUrl: 'https://staging.trade-tariff.service.gov.uk',
-      specPattern: '**/**/**/*.spec.js',
+    'chromeWebSecurity': false,
+    'firefoxWebSecurity': false,
+    'video': false,
+    'screenshotOnRunFailure': false,
+    'projectId': '7p655m',
+    'parseSpecialCharSequences': false,
+    'defaultCommandTimeout': 15000,
+    'responseTimeout': 60000,
+    setupNodeEvents(on, config) {
+      const cucumber = require('cypress-cucumber-preprocessor').default;
+      const cypressGrep = require('cypress-grep/src/plugin');
+      const dotenvPlugin = require('cypress-dotenv');
+      const {JsonSchemaValidation} = require('@jc21/cypress-jsonschema-validation');
+      const {SwaggerValidation} = require('@jc21/cypress-swagger-validation');
+
+      on('file:preprocessor', cucumber());
+      on('task', JsonSchemaValidation(config));
+      on('task', SwaggerValidation(config));
+
+      config = cypressGrep(config);
+      config = dotenvPlugin(config, {}, true);
+
+      return config;
     },
   },
 });
