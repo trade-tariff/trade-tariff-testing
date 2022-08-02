8fb0b6a045cc9d05c3bf5fbb584bcd22c1bea9f0
Madhu Dola <madviv@hotmail.com>
2022-08-02 18:37:09 +0100

    RoO e2e journeys - WO + Single agreement , RoO Custom Commands

---
+++ cypress/support/rooCommands.js
@@ -0,0 +1,80 @@
+/* eslint-disable max-len */
+// page title , dynamic content - agreements ,ticket number
+
+// Click Check rules of origin button
+Cypress.Commands.add('checkRoO', ()=>{
+  cy.contains('UK Integrated Online Tariff ');
+  cy.get('div#rules-of-origin  button[name=\'button\']').click();
+});
+// import or export
+Cypress.Commands.add('impOrExp', (country, selection)=>{
+  cy.contains('Details of your trade');
+  cy.contains(`Are you importing goods into the UK or into ${country}?`);
+  cy.get(`#rules-of-origin-steps-import-export-import-or-export-${selection}-field`).check();
+  cy.get('.govuk-button').contains('Continue').click();
+});
+// how originating is defined
+Cypress.Commands.add('howOrginating', (country, agreement)=>{
+  cy.contains('Are your goods originating?');
+  cy.contains(`How to work out if your goods are classed as 'originating' in ${country}`);
+  cy.get('.govuk-body-l').contains(`${agreement}`);
+  cy.contains(`Click on the ‘Continue’ button to view the definition of ‘wholly obtained’ in the ${agreement}.`);
+  cy.get('.govuk-button').contains('Continue').click();
+});
+
+// how wholly obtained is defined
+Cypress.Commands.add('howWhollyObtained', (agreement)=>{
+  cy.contains('Are your goods originating?');
+  cy.contains(`How 'wholly obtained' is defined in the ${agreement}`);
+  cy.contains(`Click on the ‘Continue’ button to see which parts and processes should be included or disregarded in determining originating status for the ${agreement}.`);
+  cy.get('.govuk-button').contains('Continue').click();
+});
+
+Cypress.Commands.add('whatComponents', (agreement)=>{
+  cy.contains('Are your goods originating?');
+  cy.contains('What components do you need to take into account?');
+  cy.contains('In determining whether a product originates in a given country, you should disregard the origin of neutral elements, meaning equipment and materials used in the production, but not physically incorporated into the final product, such as fuel, tools or machinery.');
+  cy.contains(`Neutral elements in the ${agreement}`);
+  cy.contains(`Packing materials in the ${agreement}`);
+  cy.contains(`Accessories, spare parts and tools in the ${agreement}`);
+  cy.contains(`Click on the 'Continue' button to indicate if your goods are wholly obtained, based on the rules of the ${agreement}.`);
+  cy.get('.govuk-button').contains('Continue').click();
+});
+// Are your goods Wholly obtained?
+Cypress.Commands.add('whollyObtained', (country, select)=>{
+  cy.contains('Are your goods originating?');
+  cy.contains(`Are your goods wholly obtained in ${country}`);
+  cy.get(`#rules-of-origin-steps-wholly-obtained-wholly-obtained-${select}-field`).check();
+  cy.get('.govuk-button').contains('Continue').click();
+});
+// Origin requirements met
+Cypress.Commands.add('originMet', (country, code, agreement)=>{
+  cy.contains('Origin requirements met');
+  cy.contains(`Importing commodity ${code} from ${country}`);
+  cy.contains(`Based on your responses, your product appears to meet the rules of origin requirements for the ${agreement}.`);
+  // links
+  cy.get('.govuk-list').contains('See valid proofs of origin').click();
+  cy.contains('Valid proofs of origin');
+  cy.get('.govuk-back-link').click();
+
+  cy.get('.govuk-list').contains('See detailed processes and requirements for proving the origin for goods').click();
+  cy.contains('Obtaining and verifying proofs of origin');
+  cy.get('.govuk-back-link').click();
+
+  cy.get('.govuk-list').contains('How proofs of origin are verified').click();
+  cy.contains('Obtaining and verifying proofs of origin');
+  cy.get('.govuk-back-link').click();
+})
+
+// not Wholly Obtained
+Cypress.Commands.add('notWhollyObtained', (country)=>{
+  cy.contains('Are your goods originating?t');
+  cy.contains('Your goods are not wholly obtained');
+  cy.contains(`If your product is not wholly obtained in ${country}, it will have to comply with other product-specific rules.`);
+});
+// Origin requirement not met
+Cypress.Commands.add('OrgReqMet', (country)=>{
+  cy.contains(`Importing commodity 6004100091 from ${country}`);
+  cy.contains('Origin requirements met');
+  cy.contains(`Based on your responses, your product appears to meet the rules of origin requirements for the UK-Vietnam Free Trade Agreement.`);
+});
