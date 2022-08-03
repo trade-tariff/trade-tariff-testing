caf1ce39296698a6ba07324c6469b78a910c1bc1
Madhu Dola <madviv@hotmail.com>
2022-08-03 12:47:26 +0100

    RoO e2e journeys - WO+multipleschemes+GSP , WO+multipleschemes+nonGSP

---
+++ cypress/e2e/RulesOfOrigin/RoO-e2e-WhollyObtained/RoO-e2e-WO-Multiple-nonGSP.spec.js
@@ -0,0 +1,22 @@
+// WO - Multiple schemes - nonGSP
+describe('| RoO-e2e-WO-Multiple-nonGSP.spec | WO + Multiple Schemes + nonGSP - Vietnam |', function() {
+  it('WO + Multiple Schemes + GSP - Vietnam', function() {
+    cy.visit('/commodities/0201100021?country=VN#rules-of-origin');
+    // click Check Rules of Origin button
+    cy.checkRoO();
+    // Select one agreement
+    cy.multipleAgreements('Vietnam', 'vietnam');
+    // Import
+    cy.impOrExp('Vietnam', 'import');
+    // How Originating is defined
+    cy.howOrginating('Vietnam', 'UK-Vietnam Free Trade Agreement');
+    // How wholly obtained is defined
+    cy.howWhollyObtained('UK-Vietnam Free Trade Agreement');
+    // what components
+    cy.whatComponents('UK-Vietnam Free Trade Agreement');
+    // Wholly Obtained ?
+    cy.whollyObtained('Vietnam', 'yes');
+    // Origin requirements met
+    cy.originMet('Vietnam', '0201100021', 'UK-Vietnam Free Trade Agreement');
+  });
+});
