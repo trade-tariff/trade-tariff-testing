94fb3e76ef2eec423ee9ee89f07b54e0fb486978
Bhavani Bandla <bhavaniprasad.bandla@enginegroup.com>
2022-09-29 14:07:01 +0100

    Revert "adding tags to not to run specific dev tests on staging and production"
    This reverts commit fabf784ded05e0af346133a64e80f1c77f08ce1c.

--- cypress/e2e/Search-Shared/searchTariff-UKXI.spec.js
+++ cypress/e2e/Search-Shared/searchTariff-UKXI.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 describe(' 🇬🇧 💡 🔍  | searchTariff-UKXI | Search the Tariff - UK and XI |', {tags: ['config', 'xbrowser-tag']}, function() {
@@ -84,28 +83,5 @@ describe(' 🇬🇧 💡 🔍  | searchTariff-UKXI | Search the Tariff - UK and 
       cy.visit(`${countries[j]}/commodities/0713900000`);
       cy.contains('Subheading 071390 - Other');
     });
-    // Search strategic - implement guides on the UI - HOTT-2002
-    const search_items = ['potatoes (fresh)', 'computers'];
-    for (let j=0; j < search_items.length; j++) {
-      it(`${countries[j]} - Beta Search - Search using name of the goods - ${search_items[j]} - implement guides on the UI`, {tags: ['devOnly']}, function() {
-        cy.visit(`${countries[j]}/find_commodity`);
-        cy.contains('Look up commodity codes, import duties, taxes and controls'); ;
-        cy.contains('Search for a commodity');
-        cy.searchForCommodity(`${search_items[j]}`);
-        cy.get('.image-guide').should('exist');
-        cy.get('#search-filter-navigation').contains('Classification guide');
-        if (`${search_items[j]}` === 'potatoes (fresh)') {
-          cy.get('#search-filter-navigation').contains('Edible fruits, nuts and peel');
-          cy.get('#search-filter-navigation').contains('Get help to classify edible vegetables, roots, tubers, herbs, spices, fruit, nuts and peel for import and export.');
-          cy.get('#search-filter-navigation').contains('View classification guide for Edible fruits, nuts and peel (opens in new tab)');
-          cy.get('#search-filter-navigation div div p a').should('have.attr', 'href', 'https://www.gov.uk/guidance/classifying-edible-fruits-nuts-and-peel');
-        } else {
-          cy.get('#search-filter-navigation').contains('Computers and software');
-          cy.get('#search-filter-navigation').contains('Get help classifying computers, inkjet and toner cartridges and their components.');
-          cy.get('#search-filter-navigation').contains('View classification guide for Computers and software (opens in new tab)');
-          cy.get('#search-filter-navigation div div p a').should('have.attr', 'href', 'https://www.gov.uk/guidance/classifying-computers-and-software');
-        }
-      });
-    }
   }
 });
