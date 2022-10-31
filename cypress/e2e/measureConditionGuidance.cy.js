e1326e73ef4857db9bd5d8dcf10d7452669f2409
William Fish <william.michael.fish@gmail.com>
2022-10-31 09:25:53 +0000

    HOTT-2178: Add support for import/export guidance checks
    - Removes an unused command
    - Renames file to reflect we're checking both services
    - Adds extra command to validate import/export modal references

--- cypress/e2e/measureConditionGuidance.cy.js
+++ cypress/e2e/measureConditionGuidance.cy.js
@@ -2,16 +2,16 @@ describe('Measure condition popup CHIEF/CDS guidance', function() {
   context('when on the UK service', function() {
     it('shows guidance', function() {
       cy.visit('/commodities/6403990510#import');
-      cy.get('#measure-20174652').contains('Conditions').click().wait(200);
-      cy.commodityGuidance();
+      cy.commodityImportGuidance();
+      cy.commodityExportGuidance();
     });
   });
 
   context('when on the XI service', function() {
     it('shows guidance', function() {
       cy.visit('/xi/commodities/6403990510#import');
-      cy.get('#measure-20174652').contains('Conditions').click().wait(200);
-      cy.commodityGuidance();
+      cy.commodityImportGuidance();
+      cy.commodityExportGuidance();
     });
   });
 });
