ad656cb1a7ca798b9a9dce43f4d2db88afd1c463
jebw <jeb@jdwilkins.co.uk>
2022-12-12 11:12:22 +0000

    HOTT-2295 Added cypress OTP

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,12 @@ const dotenvPlugin = require('cypress-dotenv');
 const {JsonSchemaValidation} = require('@jc21/cypress-jsonschema-validation');
 const {SwaggerValidation} = require('@jc21/cypress-swagger-validation');
 
+module.exports = on => {
+  on("task", {
+    generateOTP: require("cypress-otp")
+  });
+};
+
 module.exports = defineConfig({
   e2e: {
     'baseUrl': 'https://staging.trade-tariff.service.gov.uk',
@@ -13,6 +19,7 @@ module.exports = defineConfig({
 
       on('task', JsonSchemaValidation(config));
       on('task', SwaggerValidation(config));
+      on("task", { generateOTP: require("cypress-otp") });
       config = dotenvPlugin(config, {}, true);
       config = cypressGrep(config);
 
