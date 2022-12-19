dac37654f8606caece1da33dab08e415d9de116b
William Fish <william.michael.fish@gmail.com>
2022-12-19 13:54:54 +0000

    Fix broken globalSearch tests

--- cypress/e2e/HOTT-Shared/globalSearch.cy.js
+++ cypress/e2e/HOTT-Shared/globalSearch.cy.js
@@ -1,17 +1,14 @@
 /* eslint-disable max-len */
 describe('globalSearch.spec| global search function on UK and XI service ** Unreleased **', {tags: ['config', 'unreleased-tag']}, {tags: ['config', 'xbrowser-tag']}, function() {
   it('Availabilty all pages', function() {
-    cy.visit('/find_commodity');
-    cy.get('.tariff-search-banner__toggle').click();
-    cy.contains('Search the Online Tariff');
-    cy.get('.govuk-heading-m').contains('Search for a commodity');
-
-    // search for commodity by name
-    cy.globalSearchForCommodity('gherkins');
-    cy.contains('Search results for ‘gherkins’');
     // search for commodity by number
-    cy.get('.tariff-search-banner__toggle').click();
+    cy.visit('/find_commodity');
     cy.globalSearchForCommodity('0702000007');
-    cy.contains('Commodity 0702000007');
+    cy.url().should( 'include', '/commodties/0702000007');
+
+    // search for commodity by search reference
+    cy.visit('/find_commodity');
+    cy.globalSearchForCommodity('gherkins');
+    cy.url().should( 'include', '/commodties/0707009000');
   });
 });
