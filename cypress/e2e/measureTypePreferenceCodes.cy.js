8adadd28d9938f523161eaab9ee1d7642594bc33
Bhavani Bandla <bhavaniprasad.bandla@enginegroup.com>
2023-01-18 14:27:54 +0000

    add new tests for preference codes country specific

--- cypress/e2e/measureTypePreferenceCodes.cy.js
+++ cypress/e2e/measureTypePreferenceCodes.cy.js
@@ -1,5 +1,5 @@
 describe('measure type preference codes', function() {
-  it('should generate a link that is navigable', function() {
+  it('should generate a link that is navigatable', function() {
     cy.visit('/commodities/1516209821');
 
     cy.get('#measure-20001695 > td > span > a')
@@ -15,5 +15,50 @@ describe('measure type preference codes', function() {
         'include',
         '/measure_types/103/preference_codes/100?geographical_area_id=1011',
     );
+  });
+  it('preference code - show 140 when country is selected as china', function() {
+    cy.visit('/commodities/8714995019?country=CN#');
+
+    cy.get('#measure-20012253 > td > span > a')
+        .contains('Third country duty')
+        .should(
+            'have.attr',
+            'href',
+            '/measure_types/103/preference_codes/140?country=CN&geographical_area_id=1011',
+        )
+        .click();
+    cy.get('#content > a').contains('8714995019');
+    cy.get('#main-content > div').contains('103');
+    cy.get('#main-content > div').contains('140');
+  });
+  it('preference code - show 100 when no country is selected', function() {
+    cy.visit('/commodities/8714995019');
+
+    cy.get('#measure-20012253 > td > span > a')
+        .contains('Third country duty')
+        .should(
+            'have.attr',
+            'href',
+            '/measure_types/103/preference_codes/100?geographical_area_id=1011',
+        )
+        .click();
+    cy.get('#content > a').contains('8714995019');
+    cy.get('#main-content > div').contains('103');
+    cy.get('#main-content > div').contains('100');
+  });
+  it('preference code - show 100 when country is selected other than china', function() {
+    cy.visit('/commodities/8714995019?country=AD#');
+
+    cy.get('#measure-20012253 > td > span > a')
+        .contains('Third country duty')
+        .should(
+            'have.attr',
+            'href',
+            '/measure_types/103/preference_codes/100?country=AD&geographical_area_id=1011',
+        )
+        .click();
+    cy.get('#content > a').contains('8714995019');
+    cy.get('#main-content > div').contains('103');
+    cy.get('#main-content > div').contains('100');
   });
 });
