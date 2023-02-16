bacf5e64608a9b71536f1e05df799470aca70aa7
William Fish <william.michael.fish@gmail.com>
2023-02-16 12:09:55 +0000

    Removes duplicating e2e suite test

--- cypress/e2e/Search-Shared/searchScenarios.cy.js
+++ cypress/e2e/Search-Shared/searchScenarios.cy.js
@@ -73,9 +73,4 @@ describe('| searchScenarios.spec.js | UK and XI services |', function() {
       // A user clicks a 4-digit declarable code (commodity).
     }
   });
-  it('UK - subheading from A-Z directs to correct subheading page', function() {
-    cy.visit('/a-z-index/a');
-    cy.contains('Abscess Needles, Dental').click();
-    cy.contains('Subheading 901841 - Other instruments and appliances, used in dental sciences');
-  });
 });
