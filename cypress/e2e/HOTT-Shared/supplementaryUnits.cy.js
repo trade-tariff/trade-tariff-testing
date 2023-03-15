40a1abbd2c3465ab072fc033c35774b0f617be1f
William Fish <william.michael.fish@gmail.com>
2023-03-15 12:26:07 +0000

    HOTT-2849: Adds validations of supplementary units on a commodity

---
+++ cypress/e2e/HOTT-Shared/supplementaryUnits.cy.js
@@ -0,0 +1,35 @@
+describe('commodity supplementary unit classifications', function() {
+  context('when on the UK service', function() {
+    it('shows the correct supplementary units classification', function() {
+      cy.visit('/uk/commodities/6401929000');
+      cy.contains('Number of pairs (pa)');
+    });
+
+    it('shows the correct excise units classification', function() {
+      cy.visit('/uk/commodities/2402900000');
+      cy.contains('(1000 items, retail price)');
+    });
+
+    it('shows the correct no units classification', function() {
+      cy.visit('/uk/commodities/6406109010');
+      cy.contains('There are no supplementary unit measures');
+    });
+  });
+
+  context('when on the XI service', function() {
+    it('shows the correct supplementary units classification', function() {
+      cy.visit('/xi/commodities/6401929000');
+      cy.contains('Number of pairs (pa)');
+    });
+
+    it('shows the correct excise units classification', function() {
+      cy.visit('/uk/commodities/2402900000');
+      cy.contains('(1000 items, retail price)');
+    });
+
+    it('shows the correct no units classification', function() {
+      cy.visit('/uk/commodities/6406109010');
+      cy.contains('There are no supplementary unit measures');
+    });
+  });
+});
