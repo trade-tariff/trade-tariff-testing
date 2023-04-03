2f18ab56a911246cdca5222dd62dfa630a2b61d1
Bhavani Prasad Bandla <55087133+bhavanibandla@users.noreply.github.com>
2023-04-03 03:47:32 -0700

    Revert "BAU: Regression fixes"

--- cypress/e2e/UK/FE/pages-UK.cy.js
+++ cypress/e2e/UK/FE/pages-UK.cy.js
@@ -163,9 +163,9 @@ describe('🇬🇧 💡 | pages-UK.spec | Main Page - headers ,sections  - (UK v
   it('UK - import tab', function() {
     cy.visit('commodities/0702000007?day=10&month=12&year=2022#import');
     cy.contains('Importing into the UK');
-    cy.get('a[href^=\'/trading_partners\']').click();
+    cy.get('#content > dl > div:nth-child(5) > dd.govuk-summary-list__actions > a').click();
     cy.countryPickerpage({value: 'Argentina'});
-    cy.get('.govuk-summary-list__value').contains('Argentina').should('be.visible');
+    cy.get('#content > dl > div:nth-of-type(5) > .govuk-summary-list__value').contains('Argentina');
 
     cy.get('.autocomplete__wrapper').contains('Argentina (AR)');
     cy.get('a#tab_import').click();
