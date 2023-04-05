9fe8e36963c185810b9b09a99dce0f8198f75e31
Bhavani Prasad Bandla <bhavaniprasad.bandla@transformuk.com>
2023-04-05 15:50:11 +0100

    Updates ROW-GB204-e2e.spec file

--- cypress/e2e/DutyCalculator/2.RoW-GB/RoW-GB204-e2e.cy.js
+++ cypress/e2e/DutyCalculator/2.RoW-GB/RoW-GB204-e2e.cy.js
@@ -9,7 +9,7 @@ describe('| RoW-GB204-e2e.spec | 🇦🇫🇸Afghanistan to 🇬🇧 GB  |', fun
     cy.originList({value: 'Afghanistan'});
     cy.customsValue({monetary: '500.00', shipping: '100.00', cost: '250.00'});
     //  cy.quantity({tne:'1',kgm:'1',dap:'1'})
-    cy.additionalCode({uk: '2601'});
+    cy.additionalCode({uk: 'none'});
     cy.docCode({uk: 'c990'});
     cy.contains('Continue').click();
     cy.docCode({uk: 'c119'});
@@ -21,28 +21,25 @@ describe('| RoW-GB204-e2e.spec | 🇦🇫🇸Afghanistan to 🇬🇧 GB  |', fun
     cy.contains(' VAT');
     cy.contains('Option 1: Third-country duty');
     cy.contains('Option 2: Tariff preference - GSP – Least Developed Countries');
+    cy.contains('Option 3: Airworthiness tariff suspension');
     cy.contains('Option 4: Suspension - goods for certain categories of ships, boats and');
     cy.contains('other vessels and for drilling or production platforms');
-    cy.contains('Option 5: Airworthiness tariff suspension');
   });
   it(`e2e RoW to GB | 🇦🇫 Afghanistan to 🇬🇧 GB | Excise code | Wine |`, function() {
     // select future date
     cy.visit(`/duty-calculator/uk/2204210600/import-date`);
-
     cy.validDate();
     cy.selectDestination('gb');
     cy.originList({value: 'Afghanistan'});
     cy.customsValue({monetary: '500.00', shipping: '250.00', cost: '250.00'});
     cy.quantity({ltr: '1', lpa: '1'});
     // Excise code
+    cy.contains('419 - Wine with an alcoholic strength exceeding 22% ABV');
     cy.exciseCode('419');
-
     cy.confirmPage();
     cy.dutyPage();
-
     cy.contains('Option 1: Third-country duty');
     cy.contains('Option 2: Tariff preference - GSP – Least Developed Countries');
-    cy.contains('419 - Wine with an alcoholic strength exceeding 22% ABV');
   });
   it(`e2e RoW to GB | 🇫🇷 France to 🇬🇧 GB | Excise code | Champagne |`, function() {
     // select future date
@@ -53,15 +50,15 @@ describe('| RoW-GB204-e2e.spec | 🇦🇫🇸Afghanistan to 🇬🇧 GB  |', fun
     cy.customsValue({monetary: '4000.00', shipping: '500.00', cost: '500.00'});
     cy.quantity({ltr: '100'});
     // Excise code
+    cy.contains('411 - Sparkling wine of fresh grape with an alcoholic strength exceeding 8.5% ABV but not exceeding 15% ABV');
     cy.exciseCode('411');
     cy.confirmPage();
     cy.dutyPage();
     cy.contains('Option 1: Third-country duty');
     cy.contains('Option 2: Tariff preference - European Union');
-    cy.contains('£1,457.38');
-    cy.contains('£1,488.58');
+    cy.contains('£5,000.00');
+    cy.contains('£1,000.00');
     cy.contains('100 litres');
     cy.contains('Import quantity');
-    cy.contains('411 - Sparkling wine of fresh grape with an alcoholic strength exceeding 8.5% ABV but not exceeding 15% ABV');
   });
 });
