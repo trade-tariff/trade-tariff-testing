150030406415d3ed8dcbd0436411789aec9b0c4f
Bhavani Prasad Bandla <bhavaniprasad.bandla@transformuk.com>
2023-05-30 14:02:49 +0100

    adds RVC checks for RoO wholly obtained page

--- cypress/e2e/RulesOfOrigin/relatedContentRoO.cy.js
+++ cypress/e2e/RulesOfOrigin/relatedContentRoO.cy.js
@@ -37,4 +37,43 @@ describe('| relatedContentRoO - Related Content - links ', {tags: ['config', 'ro
 
     cy.contains('EU trade relations with Chile. Facts, figures and latest developments.');
   });
+  it('UK - RVC-related imagery for AU trade agreement', function() {
+    const agreement = 'Free Trade Agreement between the United Kingdom of Great Britain and Northern Ireland and Australia';
+    cy.visit('/commodities/0406103010?country=AU#rules-of-origin');
+    // click Check Rules of Origin button
+    cy.checkRoO();
+    // Import
+    cy.impOrExp('Australia', 'import');
+    // How Originating is defined
+    cy.howOrginating('Australia', `${agreement}`);
+    // How wholly obtained is defined
+    cy.contains('Are your goods originating?');
+    cy.contains(`How 'wholly obtained' is defined in the ${agreement}`);
+    cy.contains('Click on the ‘Continue’ button to see which parts and processes should be included or ');
+    cy.contains(`disregarded in determining originating status for the ${agreement}.`);
+    cy.url().should('include', '/rules_of_origin/0406103010/AU/wholly_obtained_definition');
+    cy.contains('Regional Value Content');
+    cy.contains('Build-Down Method');
+    cy.contains('RVC');
+    cy.contains('Build-Up Method');
+  });
+  it('UK - RVC-related imagery for NZ trade agreement', function() {
+    const agreement = 'Free Trade Agreement between the United Kingdom of Great Britain and Northern Ireland and New Zealand';
+    cy.visit('/commodities/0406103010?country=NZ#rules-of-origin');
+    // click Check Rules of Origin button
+    cy.checkRoO();
+    // Import
+    cy.impOrExp('New Zealand', 'import');
+    // How Originating is defined
+    cy.howOrginating('New Zealand', `${agreement}`);
+    // How wholly obtained is defined
+    cy.contains('Are your goods originating?');
+    cy.contains(`How 'wholly obtained' is defined in the ${agreement}`);
+    cy.contains('Click on the ‘Continue’ button to see which parts and processes should be included or ');
+    cy.contains(`disregarded in determining originating status for the ${agreement}.`);
+    cy.url().should('include', '/rules_of_origin/0406103010/NZ/wholly_obtained_definition');
+    cy.contains('Regional Value Content');
+    cy.contains('RVC');
+    cy.contains('Build-Up Method');
+  });
 });
