e509db20610968201c9ced8f5b962c89151e3432
Bhavani Prasad Bandla <bhavaniprasad.bandla@transformuk.com>
2023-09-19 16:41:15 +0100

    fixes duty calculator failures

--- cypress/e2e/DutyCalculator/dcShared/dcTurnOver.cy.js
+++ cypress/e2e/DutyCalculator/dcShared/dcTurnOver.cy.js
@@ -16,7 +16,7 @@ describe('🧮 | dcTurnOver.spec | Duty Calculator TurnOver page  |', function()
     cy.contains('What was your annual turnover in the most recent complete financial year?');
     cy.contains('If the turnover of your company was less than £500,000 in the latest complete financial year, ');
     cy.contains('then your goods will not be considered to be subject to commercial processing, and therefore ');
-    cy.contains('not \'at risk\' of subsequently being moved into Ireland.');
+    cy.contains('\'not at risk\' of subsequently being moved into Ireland.');
     cy.contains('My company\'s turnover was less than £500,000');
     cy.contains('My company\'s turnover was £500,000 or more');
     // check for error messages
