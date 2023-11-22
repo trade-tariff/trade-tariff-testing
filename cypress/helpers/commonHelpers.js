ee1c63e2a30bcc48b64555f369bb5dfdf6963b32
Bhavani Prasad Bandla <bhavaniprasad.bandla@transformuk.com>
2023-11-22 13:15:33 +0000

    updates to search spec files

--- cypress/helpers/commonHelpers.js
+++ cypress/helpers/commonHelpers.js
@@ -3,15 +3,37 @@ import commonPage from '../pages/commonPage';
 import searchPage from '../pages/searchPage';
 
 class CommonHelpers {
+  // search shows expected chemicals
   searchChemicals(searchstr, include, containstxt) {
     searchPage.searchForCommodity(searchstr);
     commonPage.verifyUrlShudInclude(include);
     commodityPage.clkChemicalsTab();
     commodityPage.verifyChemicalsContains(containstxt);
   }
+  // search returns results for synonyms
+  searchReturnsResultsForSynonmys(searchstr, containstxt) {
+    searchPage.searchForCommodity(searchstr);
+    searchPage.verifySearchResults(containstxt);
+  }
+  // search shows expected results
   searchNavigates(searchstr, include) {
     searchPage.enterTxtInTheSearchFieldAndClkEnter(searchstr);
     commonPage.verifyUrlShudInclude(include);
+  }
+  // search navigates to 6/8/10 digits subheadings
+  searchNavigatesToSubheadings(searchstr, suggestiontxttomatch, matchstr) {
+    searchPage.verifySearchSuggstnListAndClkSpecificSuggstnTxt(searchstr, suggestiontxttomatch);
+    commonPage.verifyUrlShudMatch(matchstr);
+  }
+  // search with expired subheadings
+  searchNavigatesExpiredSubheadings(searchstr, include) {
+    searchPage.enterTxtInTheSearchFieldAndClkEnter(searchstr);
+    commonPage.verifyUrlShudMatch(include);
+  }
+  // search with nonsense input
+  searchShowsNoResults(searchstr, containstxt) {
+    searchPage.enterTxtInTheSearchFieldAndClkEnter(searchstr);
+    commonPage.verifyContains(containstxt);
   }
 }
 
