b5cb85408fb2f635d2f1fb64f4d015436eeb73da
sunithavorsu <140505496+sunithavorsu@users.noreply.github.com>
2024-02-05 11:45:40 +0000

    fix

--- cypress/e2e/RulesOfOrigin/relatedContentRoO.cy.js
+++ cypress/e2e/RulesOfOrigin/relatedContentRoO.cy.js
@@ -1,5 +1,5 @@
 // HOTT- 954
-describe('| relatedContentRoO - Related Content - links ', { tags: ['config', 'roo-tag'] }, function () {
+describe('| relatedContentRoO - Related Content - links ', {tags: ['config', 'roo-tag']}, function() {
   it('| XI Service - EU Trading relations with Chile - Related Content |', function() {
     cy.visit('xi/commodities/0702000007');
     cy.pickTradingPartner('Chile');
@@ -10,14 +10,14 @@ describe('| relatedContentRoO - Related Content - links ', { tags: ['config', 'r
     cy.contains('Find out the rules');
     cy.go('back');
     cy.get('nav[role=\'navigation\'] > ul > li:nth-of-type(2) > a').contains('Moving goods into, out of, or through Northern Ireland')
-      .click();
+          .click();
     cy.contains('Moving goods into, out of, or through Northern Ireland.');
     cy.go('back');
     cy.get('nav[role=\'navigation\'] > ul > li:nth-of-type(3) > a').contains('Rules of Origin - Daera').click();
     cy.contains('Since 1st January 2021, trading between the UK and the EU');
     cy.go('back');
     cy.get('nav[role=\'navigation\'] > ul > li:nth-of-type(4) > a')
-      .contains('Northern Ireland Protocol: application of EU preferential rules of origin');
+          .contains('Northern Ireland Protocol: application of EU preferential rules of origin');
     cy.get('nav[role=\'navigation\'] > ul > li:nth-of-type(5) > a').contains('The EU\'s trading relationship with Chile').click();
     cy.contains('EU trade relations with Chile. Facts, figures and latest developments.');
   });
