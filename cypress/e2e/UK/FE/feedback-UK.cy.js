f20f2771b03992ecc5f710e3556582d534c7a2c9
sunithavorsu <140505496+sunithavorsu@users.noreply.github.com>
2024-02-16 15:43:05 +0000

    fixed lint issues

--- cypress/e2e/UK/FE/feedback-UK.cy.js
+++ cypress/e2e/UK/FE/feedback-UK.cy.js
@@ -45,26 +45,28 @@ describe('🇬🇧 💡 | feedback-UK | feedback link is available and user is a
       cy.visit(`${pages[i]}`);
       cy.get('.feedback-useful-banner');
       cy.contains('is this page useful?');
-      cy.contains('div > li >.govuk-button', 'Yes').should('have.attr', 'href', '/feedback?page_useful=yes')
-      cy.contains('div > li >.govuk-button', 'No').should('have.attr', 'href', '/feedback?page_useful=no')
-      cy.contains('div > li >.govuk-button', 'Report a problem with this page').should('have.attr', 'href', '/feedback')
+      cy.contains('div > li >.govuk-button', 'Yes').should('have.attr', 'href', '/feedback?page_useful=yes');
+      cy.contains('div > li >.govuk-button', 'No').should('have.attr', 'href', '/feedback?page_useful=no');
+      cy.contains('div > li >.govuk-button', 'Report a problem with this page').should('have.attr', 'href', '/feedback');
     }
   });
   it('UK - All pages - Access Feedback page from feedback useful banner - Yes button @ bottom of OTT excludes Feedback Page', function() {
-    const pages = ['/find_commodity', '/browse', '/a-z-index/a', '/tools', '/news', '/help', '/sections/1', '/chapters/01', '/headings/0101', '/commodities/0101210000'];
-    for (let i = 0; i < pages.length; i++) {
-    cy.visit(`${pages[i]}`);
-    cy.get('.feedback-useful-banner');
-    cy.contains('is this page useful?');
-    cy.contains('div > li >.govuk-button', 'Yes').should('have.attr', 'href', '/feedback?page_useful=yes').click();
-    cy.contains('UK Integrated Online Tariff');
-    cy.contains('Give feedback on Online Trade Tariff');
-    cy.contains('.feedback-useful-banner', 'is this page useful?').should('not.exist');
-  }
+    const pages = ['/find_commodity', '/browse', '/a-z-index/a', '/tools', '/news', '/help', '/sections/1', '/chapters/01', 
+      '/headings/0101','/commodities/0101210000'];
+      for (let i = 0; i < pages.length; i++) {
+      cy.visit(`${pages[i]}`);
+      cy.get('.feedback-useful-banner');
+      cy.contains('is this page useful?');
+      cy.contains('div > li >.govuk-button', 'Yes').should('have.attr', 'href', '/feedback?page_useful=yes').click();
+      cy.contains('UK Integrated Online Tariff');
+      cy.contains('Give feedback on Online Trade Tariff');
+      cy.contains('.feedback-useful-banner', 'is this page useful?').should('not.exist');
+    }
   });
   it('UK - All pages - Access Feedback page from feedback useful banner - No button @ bottom of OTT excludes Feedback Page', function() {
-    const pages = ['/find_commodity', '/browse', '/a-z-index/a', '/tools', '/news', '/help', '/sections/1', '/chapters/01', '/headings/0101', '/commodities/0101210000'];
-    for (let i = 0; i < pages.length; i++) {
+    const pages = ['/find_commodity', '/browse', '/a-z-index/a', '/tools', '/news', '/help', '/sections/1', '/chapters/01', 
+      '/headings/0101','/commodities/0101210000'];
+      for (let i = 0; i < pages.length; i++) {
     cy.visit(`${pages[i]}`);
     cy.get('.feedback-useful-banner');
     cy.contains('is this page useful?');
@@ -72,7 +74,7 @@ describe('🇬🇧 💡 | feedback-UK | feedback link is available and user is a
     cy.contains('UK Integrated Online Tariff');
     cy.contains('Give feedback on Online Trade Tariff');
     cy.contains('.feedback-useful-banner', 'is this page useful?').should('not.exist');
-  }
+    }
   });
   it(`New Feedback Page Design`, function() {
     cy.visit(`/feedback`);
@@ -82,20 +84,16 @@ describe('🇬🇧 💡 | feedback-UK | feedback link is available and user is a
     cy.contains('Give feedback on Online Trade Tariff');
     cy.contains('Tell us how to improve our service. ');
     cy.contains('Feedback is anonymous. Do not include any personal information. ');
-    // first link
     cy.get('.form-hint > a:nth-of-type(1)').should('have.attr', 'href', '/help');
-    // enter some feedback
     cy.get('.govuk-textarea').type(' 👨🏻‍💻 Cypress Test - 🇬🇧 🇪🇺 feedback ');
     cy.get('form#new_feedback > .govuk-button').click();
     cy.contains('Feedback submitted');
     cy.contains('Thank you for your valuable feedback.');
     cy.contains('What happens next');
     cy.contains('We’ve sent your feedback to the Online Trade Tariff team');
-    //@Top of OTT
-    cy.contains('.govuk-tag','FEEDBACK').should('exist');
+    cy.contains('.govuk-tag', 'FEEDBACK').should('exist');
     cy.contains('.govuk-link', 'feedback').should('exist');
     cy.contains('Tell us what you think - your feedback will help us improve.');
-    //@Bottom of OTT
     cy.get('.feedback-useful-banner');
     cy.contains('is this page useful?');
     cy.contains('div > li >.govuk-button', 'Yes').should('have.attr', 'href', '/feedback?page_useful=yes');
