20c64df3a687398889b4b54eb89db451be3f09c2
sunithavorsu <140505496+sunithavorsu@users.noreply.github.com>
2024-03-22 15:08:26 +0000

    Test cases added for the DCTS-General-Enhanced trade agreement product-specific rules_UZ

--- cypress/e2e/RulesOfOrigin/RoO-e2e-NotWhollyObtained/601-RoO-e2e-NWO-DCTS-InsuffPro.cy.js
+++ cypress/e2e/RulesOfOrigin/RoO-e2e-NotWhollyObtained/601-RoO-e2e-NWO-DCTS-InsuffPro.cy.js
@@ -74,4 +74,46 @@ describe('|601-RoO-e2e-NWO-DCTS-InsuffProces.spec | NWO + DCTS Scheme + insuffic
     // Origin requirements met
     cy.rooNotMetGSP('Bolivia', '3903110000', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
   });
+  it('Importing -   NWO + DCTS Scheme + Sufficient processing - Uzbekistan + PSR', function() {
+    cy.visit('/commodities/5306103010?country=UZ#rules-of-origin');
+    cy.checkRoO();
+    // Import
+    cy.importGSP('5306103010', 'Uzbekistan');
+    // How Originating is defined
+    cy.howOrginating('United Kingdom', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    // How wholly obtained is defined
+    cy.howWhollyObtained('Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    // what components ?
+    cy.whatComponents('Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    // Wholly Obtained yes/no ?
+    cy.whollyObtained('Uzbekistan', 'no');
+    // Your goods are not wholly obtained
+    cy.notWhollyObtained('Uzbekistan');
+    // cumulation
+    cy.cumulation('dcts-general-enhanced', '5306103010', 'UZ', 'Developing Countries Trading Scheme (DCTS)');
+    // Min Processing NO
+    cy.minimalOps('Developing Countries Trading Scheme (DCTS)', 'no');
+    // Origin requirements met
+    cy.rooNotMetGSP('Uzbekistan', '5306103010', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    // Min Processing Yes
+    cy.go(-2);
+    cy.minimalOps('Developing Countries Trading Scheme (DCTS)', 'yes');
+    // Product subDivisions
+    cy.subDivision('5306103010', 'Flax yarn');
+    // prodSpecRules
+    cy.prodSpecRules('Spinning of natural fibres.');
+    // Origin requirements met
+    cy.originMet('Uzbekistan', '5306103010', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    cy.get('.govuk-back-link').click();
+    // prodSpecRules
+    cy.prodSpecRules('Extrusion of man-made fibres accompanied by spinning (for special conditions relating to goods made of a mixture of textile materials,');
+    // Origin requirements met
+    cy.originMet('Uzbekistan', '5306103010', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+    // product specific rules?
+    cy.get('.govuk-back-link').click();
+    // product specific rules?
+    cy.prodSpecRules('Your goods do not meet any of these rules.');
+    // Origin requirements Not met
+    cy.rooNotMetGSP('Uzbekistan', '5306103010', 'Developing Countries Trading Scheme (DCTS) - Standard and Enhanced Preferences');
+  });
 });
