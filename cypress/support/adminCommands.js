4df346c618e6bba834cc0c26f86bfb975f122066
sunithavorsu <140505496+sunithavorsu@users.noreply.github.com>
2024-05-13 11:12:50 +0100

    added the missed functionality as part of the New Category Assessment page

--- cypress/support/adminCommands.js
+++ cypress/support/adminCommands.js
@@ -429,7 +429,7 @@ Cypress.Commands.add('verifyNewCategoryAssessmentPage', (service) => {
   cy.get('#category-assessment-regulation-id-error').contains('Regulation is not present');
   cy.get('#category-assessment-regulation-role-error').contains('Regulation role is not present');
   cy.get('#category-assessment-theme-id-error').contains('Theme is not present');
-  //verify the Category Assessment hyperlink functionality
+  //verify the Category Assessment hyperlink functionality in NewCategoryAssessmentPage
   cy.get('.govuk-breadcrumbs__link').should("have.attr", "href", "/xi/green_lanes/category_assessments").click();
   cy.url().should('include', '/xi/green_lanes/category_assessments');
   cy.contains('Manage category assessments');
