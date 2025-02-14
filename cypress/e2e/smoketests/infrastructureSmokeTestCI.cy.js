4d5ab5a94d2b5c867ab498e7f480d0a81e491e29
William Fish <william.michael.fish@gmail.com>
2025-02-14 12:02:58 +0000

    BAU: Fixes only failing test

--- cypress/e2e/smoketests/infrastructureSmokeTestCI.cy.js
+++ cypress/e2e/smoketests/infrastructureSmokeTestCI.cy.js
@@ -4,15 +4,15 @@ describe('Smoke tests for infrastructure', function() {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property('git_sha1');
     });
-    // cy.request('/uk/api/v2/healthcheck').then((response) => {
-    //   expect(response.status).to.eq(200);
-    //   expect(response.body).to.have.property('git_sha1');
-    //   expect(response.body.healthy).to.eq(true);
-    // });
-    // cy.request('/xi/api/v2/healthcheck').then((response) => {
-    //   expect(response.status).to.eq(200);
-    //   expect(response.body).to.have.property('git_sha1');
-    //   expect(response.body.healthy).to.eq(true);
-    // });
+    cy.request('/uk/api/v2/healthcheck').then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('git_sha1');
+      expect(response.body.healthy).to.eq(true);
+    });
+    cy.request('/xi/api/v2/healthcheck').then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('git_sha1');
+      expect(response.body.healthy).to.eq(true);
+    });
   });
 });
